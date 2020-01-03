10051357d2ba8feea8ded34cd27fe6aee4e2d8f2
ColinCPhysna <ccasto@physna.com>
2020-01-03 15:34:04 -0500

    cleanup

--- src/@services/user.service.js
+++ src/@services/user.service.js
@@ -1,1 +1,12 @@
-import {authHeader} from 'helpers'
+import {authHeader, handleResponse} from '@helpers'
+
+const getAll = () => {
+  const requestOptions = {method: 'GET', headers: authHeader ()};
+  return fetch(`${process.env.REACT_API_KEY}/users`, requestOptions).then(handleResponse);
+}
+
+const userService = {
+  getAll
+}
+
+export {userService};
