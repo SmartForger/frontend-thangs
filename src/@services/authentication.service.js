7d755a471d76609ac3800384a59f3a154ffdecf2
ColinCPhysna <ccasto@physna.com>
2020-02-27 15:32:28 -0500

    first steps

--- src/@services/authentication.service.js
+++ src/@services/authentication.service.js
@@ -1,36 +1,37 @@
 import { BehaviorSubject } from 'rxjs';
 import { handleResponse } from '@helpers';
+import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 
 const currentUserSubject = new BehaviorSubject(
     JSON.parse(localStorage.getItem('currentUser'))
 );
 
-const login = ({ email, password }) => {
+const login = async ({ email, password }) => {
     const requestOptions = {
+        url: getApiUrl(`login`),
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+        data: JSON.stringify({ email, password }),
     };
 
-    const url = getApiUrl(`login`);
-    return fetch(url, requestOptions)
-        .then(handleResponse)
-        .then(({ refresh, access }) => {
-            const user = jwtDecode(refresh);
-            console.log(jwtDecode(refresh));
-            delete user.token_type;
-            delete user.exp;
-            delete user.jti;
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            localStorage.setItem('refreshToken', refresh);
-            localStorage.setItem('accessToken', access);
-            user.accessToken = access;
-            user.refreshToken = refresh;
-            currentUserSubject.next(user);
-
-            return user;
-        });
+    try {
+        const response = await axios(requestOptions);
+        const { refresh, access } = response.data;
+        const user = jwtDecode(refresh);
+        delete user.token_type;
+        delete user.exp;
+        delete user.jti;
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        localStorage.setItem('refreshToken', refresh);
+        localStorage.setItem('accessToken', access);
+        user.accessToken = access;
+        user.refreshToken = refresh;
+        currentUserSubject.next(user);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
 };
 
 const signup = ({ email, password }) => {
