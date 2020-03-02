d06c001c13b897fa5104af5ef60ae8fe51243d20
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-02 11:26:25 -0800

    add apollo provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
@@ -12,22 +14,28 @@ import {
 import { Home, Login, Signup, Details, Profile } from '@pages';
 import { ThangsMain, GlobalStyle } from '@style';
 
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
 const App = () => {
     return (
-        <Router>
-            <GlobalStyle />
-            <ThemeProvider theme={ThangsMain}>
-                <ThangsHeader />
-                <BackgroundImage />
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} exact />
-                <Route path="/signup/:registration" component={Signup} />
-                <Route path="/details/:id" component={Details} />
-                <Route path="/profile/:id" component={Profile} />
-                <Footer />
-            </ThemeProvider>
-        </Router>
+        <ApolloProvider client={client}>
+            <Router>
+                <GlobalStyle />
+                <ThemeProvider theme={ThangsMain}>
+                    <ThangsHeader />
+                    <BackgroundImage />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} exact />
+                    <Route path="/signup/:registration" component={Signup} />
+                    <Route path="/details/:id" component={Details} />
+                    <Route path="/profile/:id" component={Profile} />
+                    <Footer />
+                </ThemeProvider>
+            </Router>
+        </ApolloProvider>
     );
 };
 
