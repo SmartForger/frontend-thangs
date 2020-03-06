38bae1b91ad526ee2fe54c556348563de89848b2
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-05 16:12:54 -0800

    v1 changeable picture

--- src/@components/ChangeablePicture.jsx
+++ src/@components/ChangeablePicture.jsx
@@ -17,12 +17,100 @@ const HiddenInput = styled.input`
 `;
 
 export function ChangeablePicture() {
+    const [src, setSrc] = useState(null);
+    const [crop, setCrop] = useState({ unit: '%', width: 30 });
+    const [croppedImgUrl, setCroppedImgUrl] = useState(null);
+
+    function onSelectFile(e) {
+        if (e.target.files && e.target.files.length > 0) {
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+                setSrc(reader.result);
+            });
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    }
+
+    let imageRef = null;
+
+    function onImageLoaded(image) {
+        imageRef = image;
+    }
+
+    function onCropComplete(crop) {
+        setCrop(crop);
+    }
+
+    function onCropChange(crop) {
+        setCrop(crop);
+    }
+
+    async function makeClientCrop(crop) {
+        if (imageRef && crop.width && crop.height) {
+            const croppedImageUrl = await getCroppedImage(
+                imageRef,
+                crop,
+                'newFile.jpeg',
+            );
+            setCroppedImgUrl(croppedImageUrl);
+        }
+    }
+
+    function getCroppedImage(image, crop, fileName) {
+        const canvas = document.createElement('canvas');
+        const scaleX = image.naturalWidth / image.width;
+        const scaleY = image.naturalHeight / image.height;
+        canvas.width = crop.width;
+        canvas.height = crop.height;
+        const ctx = canvas.getContext('2d');
+
+        ctx.drawImage(
+            image,
+            crop.x * scaleX,
+            crop.y * scaleY,
+            crop.width * scaleX,
+            crop.height * scaleY,
+            0,
+            0,
+            crop.width,
+            crop.height,
+        );
+
+        return new Promise((resolve, reject) => {
+            canvas.toBlob(blob => {
+                if (!blob) {
+                    console.error('Canvas is empty');
+                    return;
+                }
+                blob.name = fileName;
+                window.URL.revokeObjectURL(this.fileUrl);
+                this.fileUrl = window.URL.createObjectURL(blob);
+                resolve(this.fileurl);
+            });
+        }, 'image/jpeg');
+    }
+
     return (
         <form>
             <label htmlFor="avatar">
                 <ProfilePicStyled />
             </label>
-            <HiddenInput type="file" name="Change Image" id="avatar" />
+            <HiddenInput
+                type="file"
+                name="Change Image"
+                id="avatar"
+                onChange={onSelectFile}
+            />
+            {src && (
+                <ReactCrop
+                    src={src}
+                    crop={crop}
+                    onImageLoaded={onImageLoaded}
+                    onComplete={onCropComplete}
+                    onChange={onCropChange}
+                    circularCrop={true}
+                />
+            )}
         </form>
     );
 }
