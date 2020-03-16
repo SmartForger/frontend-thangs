e51faffba1e39830baba7ce2ed3c6b62583aa70d
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-16 14:28:49 -0700

    only allow certain types for markdown

--- src/@components/Markdown.jsx
+++ src/@components/Markdown.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+const allowedTypes = [
+    'link',
+    'blockquote',
+    'thematicBreak',
+    'strong',
+    'emphasis',
+    'paragraph',
+    'break',
+    'text',
+];
+
 const Markdown = ({ children }) => {
-    return <ReactMarkdown source={children} />;
+    return (
+        <ReactMarkdown
+            source={children}
+            allowedTypes={allowedTypes}
+            unwrapDisallowed
+        />
+    );
 };
 
 export { Markdown };
