720dd88d65966f767153110e6cd2837effd598c8
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-19 11:09:58 -0700

    bring components from details page to model page

--- src/@pages/Model/ModelPage.jsx
+++ src/@pages/Model/ModelPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as R from 'ramda';
 import styled from 'styled-components';
+import { useTrail } from 'react-spring';
 import * as GraphqlService from '@services/graphql-service';
-import { Button, CommentsForModel } from '@components';
+import {
+    Button,
+    CommentsForModel,
+    TagsBox,
+    Viewer,
+    ColorPicker,
+} from '@components';
 
 const graphqlService = GraphqlService.getInstance();
 
@@ -17,21 +24,44 @@ const OwnerStyled = styled.h3`
     margin: 0;
 `;
 
+const Comments = styled(CommentsForModel)`
+    grid-area: footer;
+`;
+
 const Likes = ({ likes }) => {
     const amount = likes.filter(fields => fields.isLiked).length;
-    return (
-        <div>
-            <div>Likes: {amount}</div>
-            <ul>
-                {likes.map(({ owner: { firstName, lastName } }, i) => (
-                    <li key={i}>
-                        {firstName} {lastName}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    return <div>Likes: {amount}</div>;
 };
+
+const ViewerArea = styled.div`
+    display: grid;
+    height: 100%;
+    position: relative;
+    grid-template-columns: 85% 15%;
+    grid-template-rows: 10% 75% 15%;
+    grid-template-areas:
+        'header  header'
+        'viewer  sidebar'
+        'footer  footer';
+`;
+
+const Info = styled.div`
+    position: absolute;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-around;
+    width: 12vw;
+    height: 13vw;
+    background: white;
+    z-index: 1;
+    top: 13%;
+    right: 16.5%;
+    box-shadow: inset 0 0 0 3px black;
+
+    > div {
+        margin-left: 1vw;
+    }
+`;
 
 const Owner = ({ owner }) => {
     if (!owner) {
@@ -51,11 +81,14 @@ const HeaderStyled = styled.div`
     align-items: center;
 `;
 
-const Header = ({ owner, name }) => {
+const Header = ({ model, user }) => {
+    const { owner, name } = model;
     return (
         <HeaderStyled>
             <Name>{name}</Name>
             <Owner owner={owner} />
+            <Likes likes={model.likes} />
+            <ButtonForLikes user={user} model={model} />
         </HeaderStyled>
     );
 };
@@ -95,14 +128,170 @@ const ButtonForLikes = ({ model, user }) => {
     return <LikeButton model={model} user={user} />;
 };
 
+const ViewerContainer = styled.div`
+    box-shadow: inset 0 0 0 5px black;
+    pointer-events: none;
+    grid-area: viewer;
+    display: flex;
+
+    > div {
+        pointer-events: all;
+    }
+`;
+
+const Interactions = styled.div`
+    position: absolute;
+    display: flex;
+    width: 12vw;
+    height: 7vw;
+    bottom: 17%;
+    right: 16%;
+    background: white;
+    box-shadow: inset 0 0 0 3px black;
+`;
+
+const DisplayOptions = styled.div`
+    position: absolute;
+    bottom: 17%;
+    left: 1%;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: flex-end;
+
+    > div {
+        margin-right: 15px;
+    }
+`;
+
+const DisplayButton = styled(Button)`
+    max-width: 100%;
+    width: auto;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    grid-area: sidebar;
+    padding: 5%;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const Tags = styled.div`
+    background: green;
+    width: 90%;
+    height: 50%;
+    margin-top: 5%;
+`;
+
+const ModelViewer = ({ model, user }) => {
+    const [mode, setMode] = useState('shaded');
+    const [meshColor, setMeshColor] = useState('#FFFFFF');
+    const [wireColor, setWireColor] = useState('#000000');
+
+    const tags = [
+        { name: 'Yormy' },
+        { name: 'Grimgooorsh' },
+        { name: 'AB' },
+        { name: 'Longish' },
+        { name: 'Real long Tag' },
+        { name: 'Screw' },
+        { name: 'Bolt' },
+        { name: 'Automotive' },
+        { name: 'Clasp' },
+        { name: 'Physna' },
+        { name: 'Thangs.com' },
+        { name: 'Boat' },
+        { name: 'Trucks' },
+        { name: 'Civil Engineering' },
+        { name: '3D Printing' },
+        { name: 'Yormy' },
+    ];
+
+    const names = ['Download', 'Share', 'Match', 'Identify'];
+
+    const changeMode = targetMode => {
+        setMode(targetMode);
+    };
+
+    const changeMeshColor = (color, event) => {
+        setMeshColor(color.hex);
+    };
+
+    const changeWireColor = (color, event) => {
+        setWireColor(color.hex);
+    };
+
+    const config = { mass: 5, tension: 2000, friction: 200 };
+    const trail = useTrail(names.length, {
+        config,
+        to: { opacity: 1, transform: 'translate(0,0)' },
+        from: { opacity: -2, transform: 'translate(300%,0)' },
+    });
+
+    return (
+        <>
+            <ViewerContainer>
+                <Info>
+                    <DisplayButton
+                        onClick={() => {
+                            changeMode('shaded');
+                        }}
+                        name="Shaded"
+                    />
+                    <DisplayButton
+                        onClick={() => {
+                            changeMode('wireframe');
+                        }}
+                        name="wireframe"
+                    />
+                    <DisplayButton
+                        onClick={() => {
+                            changeMode('composite');
+                        }}
+                        name="Composite"
+                    />
+                </Info>
+                <Interactions />
+                <DisplayOptions>
+                    <ColorPicker color={meshColor} onChange={changeMeshColor} />
+                    <ColorPicker color={wireColor} onChange={changeWireColor} />
+                </DisplayOptions>
+                <Viewer
+                    url="http://127.0.0.1:8000/model"
+                    mode={mode}
+                    meshColor={meshColor}
+                    wireFrameColor={wireColor}
+                />
+            </ViewerContainer>
+            <Menu>
+                {trail.map((props, index) => {
+                    return (
+                        <Button
+                            key={names[index]}
+                            name={names[index]}
+                            style={props}
+                            maxwidth="90%"
+                            height="10%"
+                            fontSize="1.5rem"
+                        />
+                    );
+                })}
+                <Tags>
+                    <TagsBox width="100%" height="100%" data={tags} />
+                </Tags>
+            </Menu>
+        </>
+    );
+};
+
 const ModelPage = ({ model, user }) => {
     return (
-        <div>
-            <Header name={model.name} owner={model.owner} />
-            <Likes likes={model.likes} />
-            <ButtonForLikes user={user} model={model} />
-            <CommentsForModel model={model} />
-        </div>
+        <ViewerArea>
+            <Header model={model} user={user} />
+            <ModelViewer model={model} user={user} />
+            <Comments model={model} />
+        </ViewerArea>
     );
 };
 
