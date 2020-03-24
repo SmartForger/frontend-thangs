979c45c20a8696377b38f0a2518dc6d658c1c396
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-24 13:20:41 -0700

    extract fetch data

--- src/@customHooks/Models.js
+++ src/@customHooks/Models.js
@@ -15,27 +15,28 @@ const useStl = url => {
         }
         return bytes.buffer;
     }
+
+    async function fetchData() {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/model');
+            const json = await response.json();
+
+            const loader = new STLLoader();
+            const geometry = loader.parse(_base64ToArrayBuffer(json.data));
+            setData(geometry);
+            setLoading(false);
+        } catch (e) {
+            setError(e);
+        }
+    }
+
     useEffect(
         () => {
-            async function fetchData() {
-                try {
-                    const response = await fetch('http://127.0.0.1:8000/model');
-                    const json = await response.json();
-
-                    const loader = new STLLoader();
-                    const geometry = loader.parse(
-                        _base64ToArrayBuffer(json.data),
-                    );
-                    setData(geometry);
-                    setLoading(false);
-                } catch (e) {
-                    setError(e);
-                }
-            }
             fetchData();
         },
         [url],
     );
+
     return [data, loading, error];
 };
 
