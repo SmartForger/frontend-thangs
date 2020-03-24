75794d0e5c106d2364b8ea5920c0d46602dae06e
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-24 10:54:52 -0700

    extract models

--- src/@pages/Profile/index.jsx
+++ src/@pages/Profile/index.jsx
@@ -30,6 +30,29 @@ const ModelsStyled = styled.div`
     height: 100%;
 `;
 
+const Models = ({ models }) => {
+    const config = { mass: 6, tension: 2000, friction: 95, clamp: true };
+    const [trail] = useTrail(models.length, () => ({
+        config,
+        to: { transform: 'translate(0,0) scale(1)' },
+        from: { transform: 'translate(1000%,0) scale(0.6)' },
+    }));
+    return (
+        <ModelsStyled>
+            {trail.map((props, index) => (
+                <ModelDisplay
+                    style={props}
+                    width="185px"
+                    height="135px"
+                    key={index}
+                    route={models[index].route}
+                    name={models[index].attachmentName}
+                />
+            ))}
+        </ModelsStyled>
+    );
+};
+
 const Page = () => {
     const { id } = useParams();
     const mockModels = [
@@ -49,13 +72,6 @@ const Page = () => {
         { attachmentName: 'Yourgoy', route: '/details/634' },
         { attachmentName: 'Yourgoy', route: '/details/634' },
     ];
-
-    const config = { mass: 6, tension: 2000, friction: 95, clamp: true };
-    const [trail] = useTrail(mockModels.length, () => ({
-        config,
-        to: { transform: 'translate(0,0) scale(1)' },
-        from: { transform: 'translate(1000%,0) scale(0.6)' },
-    }));
 
     const graphqlService = GraphqlService.getInstance();
     const { loading, error, user } = graphqlService.useUserById(id);
@@ -82,18 +98,7 @@ const Page = () => {
         <ProfileStyle>
             <HeaderStyled />
             <ProfileSidebar user={user} isCurrentUser={isCurrentUser} />
-            <ModelsStyled>
-                {trail.map((props, index) => (
-                    <ModelDisplay
-                        style={props}
-                        width="185px"
-                        height="135px"
-                        key={index}
-                        route={mockModels[index].route}
-                        name={mockModels[index].attachmentName}
-                    />
-                ))}
-            </ModelsStyled>
+            <Models models={mockModels} />
         </ProfileStyle>
     );
 };
