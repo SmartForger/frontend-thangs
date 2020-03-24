fafa7e41448ea2661355607afa41eaaeb4cf63e8
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-24 15:14:58 -0700

    remove unused useHistory

--- src/@components/ModelDisplay.jsx
+++ src/@components/ModelDisplay.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { animated } from 'react-spring';
 import { Link } from 'react-router-dom';
