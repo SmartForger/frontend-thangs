695e3d5c695b49ef8df8d3f641123c99aa35790f
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-25 10:14:42 -0700

    initialize pendo api

--- src/@components/Analytics/Pendo.jsx
+++ src/@components/Analytics/Pendo.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { initializeAnonymous } from '@vendors/pendo';
+
+const shouldTrack = () => true;
+
+// This doesn't seem to be a secret because it is included in all the request
+// urls.
+const apiKey = '47d674c1-ab92-4edf-5c76-eb419cc0be5e';
 
 const Pendo = () => {
-    console.log('Pendo hello world');
+    if (shouldTrack()) {
+        initializeAnonymous(apiKey);
+    }
     return null;
 };
 
