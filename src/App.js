9e8570f999adaf743f36e39e0841e90b4c9e3495
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-26 14:54:38 -0700

    don't render multiple routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-import { Route, Router } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 import { ThangsHeader, Footer } from '@components';
@@ -32,14 +32,19 @@ const App = () => {
                 <ThemeProvider theme={ThangsMain}>
                     <GlobalStyle />
                     <ThangsHeader />
-                    <Route exact path="/" component={Home} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/signup" component={Signup} exact />
-                    <Route path="/signup/:registration" component={Signup} />
-                    <Route path="/details/:id" component={Details} />
-                    <Route path="/profile/:id" component={Profile} />
-                    <Route path="/model/:id" component={Model} />
-                    <Route path="*" component={Page404} status={404} />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/signup" component={Signup} exact />
+                        <Route
+                            path="/signup/:registration"
+                            component={Signup}
+                        />
+                        <Route path="/details/:id" component={Details} />
+                        <Route path="/profile/:id" component={Profile} />
+                        <Route path="/model/:id" component={Model} />
+                        <Route path="*" component={Page404} status={404} />
+                    </Switch>
                     <Footer />
                 </ThemeProvider>
             </Router>
