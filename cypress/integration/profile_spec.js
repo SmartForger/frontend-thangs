a4f8ca31282cd357be9995bb84ed96036fb1104d
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-27 08:58:17 -0700

    fix profile test

--- cypress/integration/profile_spec.js
+++ cypress/integration/profile_spec.js
@@ -10,7 +10,7 @@ describe('The Profile Page', () => {
             },
         });
         cy.visit('/profile/1');
-        cy.contains('[data-cy=loading-spinner]');
+        cy.get('[data-cy=loading-spinner]');
     });
 
     it('displays user details', () => {
