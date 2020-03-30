c92f9ed99c42406e741c81bcce4a29cabb1d0d1a
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-30 16:15:03 -0700

    pull theme into layout

--- src/@style/Layout.js
+++ src/@style/Layout.js
@@ -1,6 +1,8 @@
-import styled, { css } from 'styled-components';
+import styled, { css, ThemeProvider } from 'styled-components';
 import React from 'react';
 import { ThangsHeader } from '@components/ThangsHeader';
+import { ThangsMain } from '@style/ThangsNormal.theme.js';
+import { GlobalStyle } from '@style/Thangs.GlobalStyle';
 
 const frame = fullScreen => {
     return fullScreen
@@ -25,27 +27,29 @@ const Content = styled.div`
 
 const WithLayout = Component => props => {
     return (
-        <>
+        <ThemeProvider theme={ThangsMain}>
+            <GlobalStyle />
             <ThangsHeader />
             <Layout>
                 <Content>
                     <Component {...props} />
                 </Content>
             </Layout>
-        </>
+        </ThemeProvider>
     );
 };
 
 const WithFullScreenLayout = Component => props => {
     return (
-        <>
+        <ThemeProvider theme={ThangsMain}>
+            <GlobalStyle />
             <ThangsHeader />
             <Layout fullScreen>
                 <Content>
                     <Component {...props} />
                 </Content>
             </Layout>
-        </>
+        </ThemeProvider>
     );
 };
 export { WithLayout, WithFullScreenLayout };
--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { ApolloProvider } from '@apollo/react-hooks';
 
 import { Route, Router, Switch } from 'react-router-dom';
 
-import { ThemeProvider } from 'styled-components';
 import { Footer } from '@components';
 import { Home, Login, Signup, Profile, Model } from '@pages';
 import { Newspost } from '@pages/Newspost';
 import { Page404 } from '@pages/404';
-import { ThangsMain, GlobalStyle } from '@style';
 import { graphqlClient } from '@services';
 import { createBrowserHistory } from 'history';
 import * as pendo from '@vendors/pendo';
@@ -31,27 +29,21 @@ const App = () => {
     return (
         <ApolloProvider client={client}>
             <Router history={history}>
-                <ThemeProvider theme={ThangsMain}>
-                    <GlobalStyle />
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/signup" component={Signup} exact />
-                        <Route
-                            path="/signup/:registration"
-                            component={Signup}
-                        />
-                        <Route path="/profile/:id" component={Profile} />
-                        <Route path="/model/:id" component={Model} />
-                        <Route path="/newspost/:id" component={Newspost} />
-                        <Route
-                            path="/search/:searchQuery"
-                            component={SearchResults}
-                        />
-                        <Route path="*" component={Page404} status={404} />
-                    </Switch>
-                    <Footer />
-                </ThemeProvider>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} exact />
+                    <Route path="/signup/:registration" component={Signup} />
+                    <Route path="/profile/:id" component={Profile} />
+                    <Route path="/model/:id" component={Model} />
+                    <Route path="/newspost/:id" component={Newspost} />
+                    <Route
+                        path="/search/:searchQuery"
+                        component={SearchResults}
+                    />
+                    <Route path="*" component={Page404} status={404} />
+                </Switch>
+                <Footer />
             </Router>
         </ApolloProvider>
     );
