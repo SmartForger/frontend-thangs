0494397d7503402c6564961e84f15a908d914299
Tom Nightingale <tom@tnightingale.com>
2020-03-31 10:26:42 -0700

    Use react-avatar for client-side initials avatars. Update the ProfilePicture component as a wrapper

--- src/@components/ProfilePicture.jsx
+++ src/@components/ProfilePicture.jsx
@@ -1,10 +1,36 @@
-import styled from 'styled-components';
+import React from 'react';
+import Avatar from 'react-avatar';
 
-const ProfilePicture = styled.img`
-    background: grey;
-    border-radius: 50%;
-    height: 250px;
-    width: 250px;
-`;
+const DEFAULT_AVATAR_SIZE = '250px';
+const DEFAULT_AVATAR_COLOR = '#616168';
+
+const ProfilePicture = props => {
+    const user = props.user;
+    const size = props.size || DEFAULT_AVATAR_SIZE;
+    const color = props.color || DEFAULT_AVATAR_COLOR;
+    const avatarName = getUserFullName(user);
+    return (
+        <Avatar
+            name={avatarName}
+            src={user.profile.avatar}
+            color={color}
+            size={size}
+            round={true}
+        />
+    );
+};
 
 export { ProfilePicture };
+
+function getUserFullName(user) {
+    const { firstName, lastName, username } = user;
+    if (firstName && lastName) {
+        return `${firstName} ${lastName}`;
+    } else if (firstName) {
+        return firstName;
+    } else if (lastName) {
+        return lastName;
+    } else {
+        return username;
+    }
+}
