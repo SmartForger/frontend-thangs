7c1e811b385d5d0a42999745e5b454bb4cbd4d3a
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-31 09:02:37 -0700

    build new theme file

--- src/@style/ThangsNormal.theme.js
+++ src/@style/ThangsNormal.theme.js
@@ -9,6 +9,37 @@ export const ThangsMain = {
     headerHeight: '64px',
 };
 
+const GREY_1 = '#dbdbdf';
+const GREY_2 = '#d9d9d9';
+const GREY_3 = '#b9b9be';
+const GREY_4 = '#989898';
+const GREY_5 = '#88888b';
+const GREY_6 = '#7a7a7a';
+const GREY_7 = '#616168';
+const BLACK_1 = '#515151';
+const BLACK_2 = '#232530';
+const BLACK_3 = '#231f20';
+const BLACK_4 = '#464655';
+const YELLOW_1 = '#ffbc00';
+const YELLOW_2 = '#cd9602';
+const YELLOW_3 = '#b18002';
+const BLUE_1 = '#1cb2f5';
+const BLUE_2 = '#1b8cf8';
+const BLUE_3 = '#014d7c';
+const BLUE_4 = '#013047';
+const BROWN = '#8b6400';
+
+export const NewTheme = {
+    logoText: BLACK_4,
+    brandColor: YELLOW_1,
+    searchIcon: GREY_5,
+    deleteButton: GREY_7,
+    primaryButton: BLUE_2,
+    searchText: GREY_4,
+    textOnBrandColor: BROWN,
+    emptyImageBackground: GREY_7,
+};
+
 /** Styled for normal Pages
  *
   width: ${props => props.theme.pageWidth};
