b1645791707a5b4bb18ace25e71184054b079d71
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-02 15:51:43 -0700

    increase padding on flash

--- src/@components/Flash.jsx
+++ src/@components/Flash.jsx
@@ -5,7 +5,7 @@ export const Flash = styled.div`
     color: ${props => props.theme.flashColorText};
     background-color: ${props => props.theme.flashColor};
     border-radius: 8px;
-    padding: 8px;
+    padding: 16px 24px;
     margin-bottom: 48px;
 `;
 
