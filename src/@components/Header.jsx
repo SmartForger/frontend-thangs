43dc1259c4f8839c46a8a4579a6851ccefe79f40
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-06 15:25:16 -0700

    use font weight 500 instead of bold

--- src/@components/Header.jsx
+++ src/@components/Header.jsx
@@ -53,7 +53,7 @@ const SignIn = styled(Link)`
     font-size: 14px;
     margin-right: 30px;
     font-family: ${props => props.theme.buttonFont};
-    font-weight: bold;
+    font-weight: 500;
 `;
 
 const Button = styled.button`
@@ -65,7 +65,7 @@ const Button = styled.button`
     border-radius: 8px;
     font-family: ${props => props.theme.buttonFont};
     cursor: pointer;
-    font-weight: bold;
+    font-weight: 500;
 
     ${props => props.theme.shadow};
     ${allowCssProp};
@@ -95,7 +95,7 @@ const MatchingButton = styled.button`
     border: none;
     border-radius: 8px;
     font-family: ${props => props.theme.buttonFont};
-    font-weight: bold;
+    font-weight: 500;
     font-size: 14px;
     padding: 6px 24px 6px 32px;
 
