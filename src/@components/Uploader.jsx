10ffbd08d57d193114089831ce52ff9f5fa2fc90
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-07 10:00:06 -0700

    remove unused code

--- src/@components/Uploader.jsx
+++ src/@components/Uploader.jsx
@@ -20,20 +20,6 @@ const StyledUploadBlock = styled.div`
     color: ${props => (props.dragactive ? 'black' : 'white')};
 `;
 
-const readFileAsync = file => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-
-        reader.onerror = reject;
-
-        reader.readAsArrayBuffer(file);
-    });
-};
-
 const FileUpload = () => {
     const [draggedfiles, setDraggedFiles] = useState([]);
     const graphqlService = GraphqlService.getInstance();
@@ -58,66 +44,11 @@ const FileUpload = () => {
                 userEmail: authenticationService.currentUserValue.email,
             },
         });
-
-        // const modelFormData = new FormData();
-        // modelFormData.append('files', draggedfiles);
-
-        // await draggedfiles.forEach(async file => {
-        //     // const processedFile = {};
-        //     // processedFile.path = file.path;
-        //     // processedFile.name = file.name;
-        //     // processedFile.type = file.type;
-        //     // processedFile.size = file.size;
-        //     // processedFile.lastModified = file.lastModified;
-        //     // processedFile.lastModifiedDate = file.lastModifiedDate;
-
-        //     // const binary = await readFileAsync(file);
-
-        //     // processedFile['binaryData'] = btoa(
-        //     //     new Uint8Array(binary).reduce(
-        //     //         (data, byte) => data + String.fromCharCode(byte),
-        //     //         ''
-        //     //     )
-        //     // );
-        //     // return processedFile;
-        //     modelFormData.append(file);
-        // });
-
-        // Promise.all(encodedFiles).then(values => {
-        //     axios.post(
-        //         // `${process.env.REACT_APP_MODEL_KEY}upload_attachments`,
-        //         '/api/upload',
-        //         values,
-        //         {
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //             },
-        //         }
-        //     );
-        // });
-
-        // axios({
-        //     method: 'POST',
-        //     url: `${process.env.REACT_APP_MODEL_KEY}upload_attachments`,
-        //     data: modelFormData,
-        //     headers: {
-        //         'Content-Type': 'multipart/form-data',
-        //     },
-        // });
     };
 
     return (
         <>
             <form onSubmit={onSubmit}>
-                <div className="customFile">
-                    {/* <input
-                        type="file"
-                        className="file-input"
-                        id="custom-file"
-                        onChange={onChange}
-                    /> */}
-                </div>
-
                 <div {...getRootProps()}>
                     <input {...getInputProps()} />
                     <StyledUploadBlock dragactive={isDragActive}>
