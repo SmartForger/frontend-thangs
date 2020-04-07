c3d8b3543a8cd052d82b7c9c7aff70800c15aae4
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-07 11:40:42 -0700

    add styling for upload form

--- src/@components/Uploader.jsx
+++ src/@components/Uploader.jsx
@@ -29,8 +29,63 @@ const LinkColor = styled.span`
     color: ${props => props.theme.linkText};
 `;
 
+const Row = styled.div`
+    display: flex;
+`;
+
+const allowCssProp = props => (props.css ? props.css : '');
+
+const Column = styled.div`
+    ${allowCssProp};
+`;
+
+const Field = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const FullWidthInput = styled.input`
+    display: block;
+    flex-grow: 1;
+    border: 0;
+    padding: 8px 16px;
+    margin-bottom: 8px;
+    border-radius: 8px;
+
+    ${allowCssProp};
+`;
+
+const Label = styled.label`
+    margin-bottom: 8px;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 96px;
+`;
+
+const Button = styled.button`
+    color: ${props => props.theme.primaryButtonText};
+    background-color: ${props => props.theme.primaryButton};
+    font-size: 14px;
+    padding: 8px 36px;
+    border: none;
+    border-radius: 8px;
+    font-family: ${props => props.theme.buttonFont};
+    font-weight: 500;
+    cursor: pointer;
+
+    ${props => props.theme.shadow};
+    ${allowCssProp};
+`;
+
+const CancelButton = styled(Button)`
+    background-color: ${props => props.theme.deleteButton};
+`;
+
 function Uploader() {
-    const [draggedFile, setDraggedFile] = useState([]);
+    const [draggedFile, setDraggedFile] = useState();
     const graphqlService = GraphqlService.getInstance();
     const [uploadModel] = graphqlService.useUploadModelMutation();
 
@@ -59,18 +114,56 @@ function Uploader() {
 
     return (
         <form onSubmit={onSubmit}>
-            <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <StyledUploadBlock dragactive={isDragActive}>
-                    <UploadIconStyled />
-                    <div>Drag & Drop model</div>
-                    <div>
-                        or <LinkColor>browse</LinkColor> to choose file
+            <Row>
+                <Column
+                    css={`
+                        flex-grow: 1;
+                        margin-right: 32px;
+                    `}
+                >
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <StyledUploadBlock dragactive={isDragActive}>
+                            {draggedFile ? (
+                                <div>File: {draggedFile.name}</div>
+                            ) : (
+                                <>
+                                    <UploadIconStyled />
+                                    <div>Drag & Drop model</div>
+                                    <div>
+                                        or <LinkColor>browse</LinkColor> to
+                                        choose file
+                                    </div>
+                                </>
+                            )}
+                        </StyledUploadBlock>
                     </div>
-                </StyledUploadBlock>
-            </div>
-            {draggedFile && <div>{draggedFile.name}</div>}
-            <input type="submit" value="Upload" />
+                </Column>
+                <Column
+                    css={`
+                        min-width: 336px;
+                    `}
+                >
+                    <Field>
+                        <Label htmlFor="name">Title</Label>
+                        <FullWidthInput
+                            name="name"
+                            defaultValue={draggedFile && draggedFile.name}
+                            placeholder="Model Name"
+                        />
+                    </Field>
+                </Column>
+            </Row>
+            <ButtonGroup>
+                <CancelButton
+                    css={`
+                        margin-right: 8px;
+                    `}
+                >
+                    Cancel
+                </CancelButton>
+                <Button type="submit">Save Model</Button>
+            </ButtonGroup>
         </form>
     );
 }
