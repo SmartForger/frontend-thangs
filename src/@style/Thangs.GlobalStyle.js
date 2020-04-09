c79c23548509d7190e9e16f9d8a4c01378b77c32
Tommy Groshong <tom@groshong.software>
2020-04-09 16:13:53 -0400

    Kill old layouts, themes, and global styles

--- src/@style/Thangs.GlobalStyle.js
+++ src/@style/Thangs.GlobalStyle.js
@@ -1,26 +1,6 @@
-import { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-const enablePlaceholderForContentEditable = css`
-    [contentEditable='true']:empty:before {
-        content: attr(data-placeholder);
-    }
-`;
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Raleway');
-
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: Raleway, sans-serif;
-    overflow-x: hidden;
-    background: ${props => props.theme.primary};
-  }
-
-  ${enablePlaceholderForContentEditable}
-`;
-
-const NewGlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&family=Montserrat:wght@300;400;500;600;700&display=swap');
 
   body {
@@ -45,5 +25,3 @@ const NewGlobalStyle = createGlobalStyle`
     margin: 0;
   }
 `;
-
-export { GlobalStyle, NewGlobalStyle };
