6a644529dacd6546528d42a0de12fda9200cee34
Tommy Groshong <tom@groshong.software>
2020-04-09 15:51:31 -0400

    Update 404 styling for new layout.

--- src/@pages/404.jsx
+++ src/@pages/404.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { WithLayout } from '@style/Layout';
+import { WithNewThemeLayout } from '@style/Layout';
 
 const Page = () => {
-    return <div>We couldn't find the page you're looking for. Sorry!</div>;
+    return <h1>We couldn't find the page you're looking for. Sorry!</h1>;
 };
 
-export const Page404 = WithLayout(Page);
+export const Page404 = WithNewThemeLayout(Page);
