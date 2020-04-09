409cfe12277a32a5390bb35b3b2a2600fcf0e599
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-09 12:11:56 -0700

    extrct upload frame

--- src/@components/Uploader.jsx
+++ src/@components/Uploader.jsx
@@ -2,22 +2,7 @@ import React, { useCallback } from 'react';
 import styled from 'styled-components';
 import { useDropzone } from 'react-dropzone';
 import { ReactComponent as UploadIcon } from '@svg/upload-icon.svg';
-
-const StyledUploadBlock = styled.div`
-    height: 560px;
-    border-radius: 8px;
-    background-color: ${props =>
-        props.dragactive
-            ? props.theme.uploaderBackgroundActive
-            : props.theme.uploaderBackground};
-    color: ${props => props.theme.uploaderText};
-    font-size: 24px;
-    line-height: 36px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
+import { UploadFrame } from '@components/UploadFrame';
 
 const UploadIconStyled = styled(UploadIcon)`
     margin-bottom: 32px;
@@ -26,6 +11,7 @@ const UploadIconStyled = styled(UploadIcon)`
 const LinkColor = styled.span`
     color: ${props => props.theme.linkText};
 `;
+
 export function Uploader({ file, setFile }) {
     const onDrop = useCallback(
         acceptedFiles => {
@@ -40,7 +26,7 @@ export function Uploader({ file, setFile }) {
     return (
         <div {...getRootProps()}>
             <input {...getInputProps({ multiple: false })} />
-            <StyledUploadBlock dragactive={isDragActive}>
+            <UploadFrame dragactive={isDragActive}>
                 {file ? (
                     <div>File: {file.name}</div>
                 ) : (
@@ -52,7 +38,7 @@ export function Uploader({ file, setFile }) {
                         </div>
                     </>
                 )}
-            </StyledUploadBlock>
+            </UploadFrame>
         </div>
     );
 }
