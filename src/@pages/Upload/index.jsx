730a9b8992237c45b85a253c43faedcee49aa3fd
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-09 09:18:30 -0700

    move uploader contents to page

--- src/@pages/Upload/index.jsx
+++ src/@pages/Upload/index.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import { WithNewThemeLayout } from '@style/Layout';
 import { Uploader } from '@components/Uploader';
+import { useDropzone } from 'react-dropzone';
+import * as GraphqlService from '@services/graphql-service';
+import { authenticationService } from '@services';
+import { useHistory } from 'react-router-dom';
+import { ReactComponent as UploadIcon } from '@svg/upload-icon.svg';
+
+const StyledUploadBlock = styled.div`
+    height: 560px;
+    border-radius: 8px;
+    background-color: ${props =>
+        props.dragactive
+            ? props.theme.uploaderBackgroundActive
+            : props.theme.uploaderBackground};
+    color: ${props => props.theme.uploaderText};
+    font-size: 24px;
+    line-height: 36px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const UploadIconStyled = styled(UploadIcon)`
+    margin-bottom: 32px;
+`;
+
+const LinkColor = styled.span`
+    color: ${props => props.theme.linkText};
+`;
+
+const Row = styled.div`
+    display: flex;
+`;
+
+const allowCssProp = props => (props.css ? props.css : '');
+
+const Column = styled.div`
+    ${allowCssProp};
+`;
+
+const Field = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const FullWidthInput = styled.input`
+    display: block;
+    flex-grow: 1;
+    border: 0;
+    padding: 8px 16px;
+    margin-bottom: 8px;
+    border-radius: 8px;
+
+    ${allowCssProp};
+`;
+
+const Label = styled.label`
+    margin-bottom: 8px;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 32px;
+`;
+
+const Button = styled.button`
+    color: ${props => props.theme.primaryButtonText};
+    background-color: ${props => props.theme.primaryButton};
+    font-size: 14px;
+    padding: 8px 36px;
+    border: none;
+    border-radius: 8px;
+    font-family: ${props => props.theme.buttonFont};
+    font-weight: 500;
+    cursor: pointer;
+
+    :disabled {
+        cursor: not-allowed;
+    }
+
+    ${props => props.theme.shadow};
+    ${allowCssProp};
+`;
+
+const CancelButton = styled(Button)`
+    background-color: ${props => props.theme.deleteButton};
+`;
 
 const Header = styled.h1`
     font-family: ${props => props.theme.headerFont};
@@ -10,10 +98,101 @@ const Header = styled.h1`
 `;
 
 const Page = () => {
+    const history = useHistory();
+
+    const [draggedFile, setDraggedFile] = useState();
+    const graphqlService = GraphqlService.getInstance();
+    const [uploadModel] = graphqlService.useUploadModelMutation();
+
+    const onDrop = useCallback(
+        acceptedFiles => {
+            setDraggedFile(acceptedFiles[0]);
+        },
+        [setDraggedFile],
+    );
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+    });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        await uploadModel({
+            variables: {
+                file: draggedFile,
+                name: draggedFile.name,
+                size: draggedFile.size,
+                userEmail: authenticationService.currentUserValue.email,
+            },
+        });
+        history.push('/new/profile');
+    };
+
+    const handleCancel = e => {
+        e.preventDefault();
+        history.goBack();
+    };
+
     return (
         <div>
             <Header>Upload Model</Header>
-            <Uploader />
+            <form onSubmit={onSubmit}>
+                <Row>
+                    <Column
+                        css={`
+                            flex-grow: 1;
+                            margin-right: 32px;
+                        `}
+                    >
+                        <Uploader />
+                        <div {...getRootProps()}>
+                            <input {...getInputProps({ multiple: false })} />
+                            <StyledUploadBlock dragactive={isDragActive}>
+                                {draggedFile ? (
+                                    <div>File: {draggedFile.name}</div>
+                                ) : (
+                                    <>
+                                        <UploadIconStyled />
+                                        <div>Drag & Drop model</div>
+                                        <div>
+                                            or <LinkColor>browse</LinkColor> to
+                                            choose file
+                                        </div>
+                                    </>
+                                )}
+                            </StyledUploadBlock>
+                        </div>
+                    </Column>
+                    <Column
+                        css={`
+                            min-width: 336px;
+                        `}
+                    >
+                        <Field>
+                            <Label htmlFor="name">Title</Label>
+                            <FullWidthInput
+                                name="name"
+                                defaultValue={draggedFile && draggedFile.name}
+                                placeholder="Model Name"
+                            />
+                        </Field>
+                    </Column>
+                </Row>
+                <ButtonGroup>
+                    <CancelButton
+                        css={`
+                            margin-right: 8px;
+                        `}
+                        onClick={handleCancel}
+                        type="button"
+                    >
+                        Cancel
+                    </CancelButton>
+                    <Button type="submit" disabled={!draggedFile}>
+                        Save Model
+                    </Button>
+                </ButtonGroup>
+            </form>
         </div>
     );
 };
