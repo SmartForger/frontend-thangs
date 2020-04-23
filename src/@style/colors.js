d88b7ae2ba362b5334057197239976c57711e646
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-23 13:25:23 -0700

    extract colors from theme file

---
+++ src/@style/colors.js
@@ -0,0 +1,36 @@
+/*
+ * These colors are commented out for now to avoid unused variable errors in
+ * our build. That is because we haven't placed in the theme yet, but they are
+ * present in our designer's palette and we should account for them.
+ */
+export const OFF_WHITE = '#f5f5f5';
+export const GREY_1 = '#dbdbdf';
+// const GREY_2 = '#d9d9d9';
+export const GREY_3 = '#b9b9be';
+export const GREY_4 = '#989898';
+export const GREY_5 = '#88888b';
+export const GREY_6 = '#7a7a7a';
+export const GREY_7 = '#616168';
+export const GREY_8 = '#e6e6e9';
+export const GREY_9 = '#b9b9be';
+export const GREY_10 = '#bfbfbf';
+export const BLACK_1 = '#515151';
+export const BLACK_2 = '#232530';
+export const BLACK_3 = '#231f20';
+export const BLACK_4 = '#464655';
+export const BLACK_5 = '#4a4a4a';
+export const YELLOW_1 = '#ffbc00';
+// const YELLOW_2 = '#cd9602';
+// const YELLOW_3 = '#b18002';
+export const BLUE_1 = '#1cb2f5';
+export const BLUE_2 = '#1b8cf8';
+export const BLUE_3 = '#014d7c';
+// const BLUE_4 = '#013047';
+export const BROWN = '#8b6400';
+export const RED_1 = '#edd0d0';
+export const RED_4 = '#ca2d2c';
+export const WHITE_1 = '#ffffff';
+export const WHITE_2 = '#efecec';
+export const WHITE_3 = '#e8e8ec';
+export const WHITE_4 = '#ececec';
+export const LIGHT_PURPLE = '#dd72dd';
