f86837fd843f62e531e72edc84b6cdb4dd8bb0de
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-23 13:29:24 -0700

    scaffold texts file

---
+++ src/@style/text.js
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+export const modelCardHoverText = css``;
+export const flashToastText = css``;
+export const zeroStateText = css``;
+export const tabNavigationText = css``;
+export const formCalloutText = css``;
+export const usernameText = css``;
+export const subheaderText = css``;
+export const modelTitleText = css``;
+export const pageTitleText = css``;
+export const headerText = css``;
+export const logoText = css``;
