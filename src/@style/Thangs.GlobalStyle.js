6358ec6757dab0e484b3098bbe42abcb1b04bbc2
Tommy Groshong <tom@groshong.software>
2020-04-24 18:10:17 -0400

    Use theme font and link colors to handle dark pages

--- src/@style/Thangs.GlobalStyle.js
+++ src/@style/Thangs.GlobalStyle.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-import { BLACK_1, BLUE_2 } from './colors';
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&family=Montserrat:wght@300;400;500;600;700&display=swap');
@@ -11,12 +10,12 @@ export const GlobalStyle = createGlobalStyle`
     background: ${props => props.theme.backgroundColor};
     font-family: ${props => props.theme.buttonFont};
     font-size: 14px;
-    color: ${BLACK_1}
+    color: ${props => props.theme.mainFontColor};
   }
 
   a {
     text-decoration: none;
-    color: ${BLUE_2};
+    color: ${props => props.theme.linkText};
   }
 
   h1, h2, h3, h4, h5, h6 {
