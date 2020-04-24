2ae7bda0eab493e1faacc2b9ceda2ea01cfd71b7
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-24 09:38:58 -0700

    remove old search bar

--- src/@components/SearchBar.jsx
+++ src/@components/SearchBar.jsx
@@ -3,24 +3,6 @@ import styled from 'styled-components';
 import { useHistory, useParams } from 'react-router-dom';
 import { ReactComponent as SearchIcon } from '@svg/search-icon.svg';
 
-const SearchForm = styled.form`
-    width: 50%;
-    height: 100%;
-    display: flex;
-`;
-
-const SearchStyle = styled.input`
-    border: 0.5px solid ${props => props.theme.darkgrey};
-    padding: 0 15px;
-    background: ${props => props.theme.grey};
-    width: 100%;
-    font-size: 30px;
-
-    ::placeholder {
-        color: ${props => props.theme.darkgrey};
-    }
-`;
-
 function useSearch(initialSearchQuery = '') {
     const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
     const history = useHistory();
@@ -29,34 +11,6 @@ function useSearch(initialSearchQuery = '') {
     };
     return { searchQuery, setSearchQuery, executeSearch };
 }
-
-const SearchBar = props => {
-    const params = useParams();
-    const initialSearchQuery = params.searchQuery;
-    const { searchQuery, setSearchQuery, executeSearch } = useSearch(
-        initialSearchQuery
-    );
-
-    const handleChange = e => {
-        e.persist();
-        setSearchQuery(e.target.value);
-    };
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        executeSearch();
-    };
-
-    return (
-        <SearchForm onSubmit={handleSubmit}>
-            <SearchStyle
-                placeholder="Input Search Term"
-                value={searchQuery}
-                onChange={handleChange}
-            />
-        </SearchForm>
-    );
-};
 
 const SearchFormNew = styled.form`
     flex-grow: 1;
@@ -118,4 +72,4 @@ const SearchBarNew = props => {
     );
 };
 
-export { SearchBar, SearchBarNew };
+export { SearchBarNew };
