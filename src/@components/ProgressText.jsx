a6f2f4a5b71e052013e0d881622089e7f68eaac8
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-04-24 16:13:54 -0700

    prettier fix

--- src/@components/ProgressText.jsx
+++ src/@components/ProgressText.jsx
@@ -3,19 +3,16 @@ import React, { useState, useEffect } from 'react';
 export function ProgressText({ text, className }) {
     const [dots, setDots] = useState('.');
 
-    useEffect(
-        () => {
-            const timeout = setTimeout(() => {
-                if (dots.length < 3) {
-                    setDots(`${dots}.`);
-                } else {
-                    setDots('.');
-                }
-            }, 500);
-            return () => clearTimeout(timeout);
-        },
-        [dots, setDots]
-    );
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (dots.length < 3) {
+                setDots(`${dots}.`);
+            } else {
+                setDots('.');
+            }
+        }, 500);
+        return () => clearTimeout(timeout);
+    }, [dots, setDots]);
 
     return (
         <div className={className}>
