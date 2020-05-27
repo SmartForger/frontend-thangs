126a16cab4f2b96e2a64cffa5f0eeb0dc11482c9
Tom Nightingale <tom@tnightingale.com>
2020-05-27 10:56:19 -0700

    Add DEBUG guard to debug statements

--- src/@components/RouteComponent.jsx
+++ src/@components/RouteComponent.jsx
@@ -3,14 +3,17 @@ import { Redirect } from 'react-router-dom';
 import { authenticationService } from '@services';
 import { history } from '../history';
 
+const DEBUG = process.env.REACT_APP_DEBUG;
+
 export function routeRequiresAuth(Component, redirectDest = '/login') {
     return function AuthRouteComponent(...props) {
         const isAuthed = authenticationService.getCurrentUserId() !== undefined;
         const currentPath = history.location.pathname;
         const shouldRedirect = !isAuthed && currentPath !== redirectDest;
-        console.debug(
-            `Route requires authenticated user, should redirect to ${redirectDest}? ${shouldRedirect}`
-        );
+        DEBUG &&
+            console.debug(
+                `Route requires authenticated user, should redirect to ${redirectDest}? ${shouldRedirect}`
+            );
         return shouldRedirect ? (
             <Redirect to={redirectDest} />
         ) : (
@@ -24,9 +27,10 @@ export function routeRequiresAnon(Component, redirectDest = '/') {
         const isAuthed = authenticationService.getCurrentUserId() !== undefined;
         const currentPath = history.location.pathname;
         const shouldRedirect = isAuthed && currentPath !== redirectDest;
-        console.debug(
-            `Route requires anonymous user, should redirect to ${redirectDest}? ${shouldRedirect}`
-        );
+        DEBUG &&
+            console.debug(
+                `Route requires anonymous user, should redirect to ${redirectDest}? ${shouldRedirect}`
+            );
         return shouldRedirect ? (
             <Redirect to={redirectDest} />
         ) : (
