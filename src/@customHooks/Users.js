4de571ab6081ba1c615a6fcd292a6f4b920c4223
Tom Nightingale <tom@tnightingale.com>
2020-05-27 10:39:35 -0700

    Remove unused/commented experimental code

--- src/@customHooks/Users.js
+++ src/@customHooks/Users.js
@@ -1,23 +1,7 @@
-// import { useState, useEffect } from 'react';
 import * as GraphqlService from '@services/graphql-service';
 import { authenticationService } from '@services';
 
 const graphqlService = GraphqlService.getInstance();
-
-// function useAuthenticationServiceSubscription() {
-//     const [currentUser, setCurrentUser] = useState();
-//     useEffect(() => {
-//         // Subscribe to currentUser changes
-//         const unsubscribe = authenticationService.subscribe(event => {
-//             setCurrentUser(event.user);
-//         });
-//         return function cleanup() {
-//             // Unsubscribe to currentUser changes
-//             unsubscribe();
-//         };
-//     }, []);
-//     return { user: currentUser };
-// }
 
 export function useCurrentUser() {
     const id = authenticationService.getCurrentUserId();
