acdf0e95e7352a0efd28a17cf4286e0faa6a6177
Tom Nightingale <tom@tnightingale.com>
2020-06-01 12:55:27 -0700

    Add storybooks for MODEL_COMPLETED_PROCESSING & MODEL_FAILED_PROCESSING

--- src/@components/NotificationsList/Notification.stories.jsx
+++ src/@components/NotificationsList/Notification.stories.jsx
@@ -5,8 +5,8 @@ import {
     // NOT_RECOGNIZED,
     // USER_DOWNLOADED_MODEL,
     // MODEL_CHANGED_STATUS,
-    // MODEL_FAILED_PROCESSING,
-    // MODEL_COMPLETED_PROCESSING,
+    MODEL_FAILED_PROCESSING,
+    MODEL_COMPLETED_PROCESSING,
     // USER_COMMENTED_ON_MODEL,
     // USER_UPLOADED_MODEL,
     USER_STARTED_FOLLOWING_USER,
@@ -14,6 +14,59 @@ import {
 
 const TIME = '2020-05-14T20:04:05.677850+00:00';
 
+/**
+ * MODEL_FAILED_PROCESSING
+ * Verb: "changed status"
+ * Actor: Model
+ * Target: None
+ * ActionObject: None
+ */
+export function ModelFailedProcessing() {
+    const props = {
+        timestamp: TIME,
+        actor: {
+            id: 9999,
+            thumbnailUrl: '',
+            name: '',
+        },
+        verb: 'changed status',
+        target: null,
+        notificationType: MODEL_FAILED_PROCESSING,
+        actionObject: null,
+    };
+    return <Notification {...props} />;
+}
+
+/**
+ * MODEL_COMPLETED_PROCESSING
+ * Verb: "changed status"
+ * Actor: Model
+ * Target: None
+ * ActionObject: None
+ */
+export function ModelCompletedProcessing() {
+    const props = {
+        timestamp: TIME,
+        actor: {
+            id: 9999,
+            thumbnailUrl: '',
+            name: '',
+        },
+        verb: 'changed status',
+        target: null,
+        notificationType: MODEL_COMPLETED_PROCESSING,
+        actionObject: null,
+    };
+    return <Notification {...props} />;
+}
+
+/**
+ * USER_STARTED_FOLLOWING_USER
+ * Verb: "started following"
+ * Actor: User
+ * Target: None
+ * ActionObject: None
+ */
 export function UserStartedFollowingUser() {
     const props = {
         timestamp: TIME,
