e9c1399400647a6bee25fdfd5d615725a23c4c34
Brandon Every <brandonevery@gmail.com>
2020-08-04 18:49:21 -0400

    Move hook to avoid inconsistent hook calls

--- src/@customHooks/Users.js
+++ src/@customHooks/Users.js
@@ -6,12 +6,12 @@ const graphqlService = GraphqlService.getInstance()
 
 export function useCurrentUser() {
   const id = authenticationService.getCurrentUserId()
+  const { loading, error, user } = graphqlService.useUserById(id)
   const { user: userStore } = useStoreon('user')
   if (!id) {
     return { loading: false, error: undefined, user: undefined }
   }
 
-  const { loading, error, user } = graphqlService.useUserById(id)
   if (user) user.models = [...userStore.models, ...user.models] //TEMP - This is to merge the user models cached by graphQL and new models uploaded as new versions - BE
   return { loading, error, user }
 }
