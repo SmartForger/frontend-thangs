040fac26b928672e6de5229eec7e32ebd05f3405
Brandon Every <brandonevery@gmail.com>
2020-08-05 15:56:16 -0400

    Lint updates

--- scripts/update-fragments.js
+++ scripts/update-fragments.js
@@ -4,20 +4,20 @@
  * BEFORE RUNNING: check your env file to be sure that it includes the required
  * env vars.
  */
-const axios = require('axios');
-const { promises: fs } = require('fs');
+const axios = require('axios')
+const { promises: fs } = require('fs')
 
-const SOCIAL_SERVICE_URL = process.env.SCRIPTS_SOCIAL_SERVICE_URL;
-const email = process.env.SCRIPTS_SOCIAL_SERVICE_EMAIL;
-const password = process.env.SCRIPTS_SOCIAL_SERVICE_PASSWORD;
+const SOCIAL_SERVICE_URL = process.env.SCRIPTS_SOCIAL_SERVICE_URL
+const email = process.env.SCRIPTS_SOCIAL_SERVICE_EMAIL
+const password = process.env.SCRIPTS_SOCIAL_SERVICE_PASSWORD
 
 async function getToken({ email, password }) {
-    const response = await axios.post(`${SOCIAL_SERVICE_URL}/api/login/`, {
-        email,
-        password,
-    });
-    const { access } = response.data;
-    return access;
+  const response = await axios.post(`${SOCIAL_SERVICE_URL}/api/login/`, {
+    email,
+    password,
+  })
+  const { access } = response.data
+  return access
 }
 
 const SCHEMA_QUERY = `
@@ -32,42 +32,42 @@ const SCHEMA_QUERY = `
           }
         }
       }
-    `;
+    `
 
 async function requestFragments(token) {
-    const { data: result } = await axios.post(
-        `${SOCIAL_SERVICE_URL}/graphql/`,
-        {
-            variables: {},
-            query: SCHEMA_QUERY,
-        },
-        {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        }
-    );
+  const { data: result } = await axios.post(
+    `${SOCIAL_SERVICE_URL}/graphql/`,
+    {
+      variables: {},
+      query: SCHEMA_QUERY,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
 
-    // here we're filtering out any type information unrelated to unions or interfaces
-    const filteredData = result.data.__schema.types.filter(
-        type => type.possibleTypes !== null
-    );
-    result.data.__schema.types = filteredData;
-    return result.data;
+  // here we're filtering out any type information unrelated to unions or interfaces
+  const filteredData = result.data.__schema.types.filter(
+    type => type.possibleTypes !== null
+  )
+  result.data.__schema.types = filteredData
+  return result.data
 }
 
 async function main() {
-    try {
-        const token = await getToken({ email, password });
-        const fragments = await requestFragments(token);
-        await fs.writeFile(
-            './src/@services/graphql-service/fragmentTypes.json',
-            JSON.stringify(fragments)
-        );
-        console.log('Fragment types successfully extracted!');
-    } catch (err) {
-        console.error('Error writing fragmentTypes file', err);
-    }
+  try {
+    const token = await getToken({ email, password })
+    const fragments = await requestFragments(token)
+    await fs.writeFile(
+      './src/@services/graphql-service/fragmentTypes.json',
+      JSON.stringify(fragments)
+    )
+    console.log('Fragment types successfully extracted!')
+  } catch (err) {
+    console.error('Error writing fragmentTypes file', err)
+  }
 }
 
-main();
+main()
