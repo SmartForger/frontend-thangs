707dac2da47858e3e2201d6e2d4c5af1cca20c45
Brandon Every <brandonevery@gmail.com>
2020-08-07 02:02:34 -0400

    Update components with hooks and export defaults

--- src/@components/SearchBar.jsx
+++ src/@components/SearchBar.jsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, { useCallback, useState } from 'react'
+=======
+import React, { useCallback, useState, useMemo } from 'react'
+>>>>>>> 1ea4785... Update components with hooks and export defaults
 import { useHistory } from 'react-router-dom'
 import { ReactComponent as SearchIcon } from '@svg/search-icon.svg'
 import classnames from 'classnames'
@@ -51,9 +55,9 @@ const useStyles = createUseStyles(theme => {
 const useSearch = (initialSearchQuery = '') => {
   const [searchQuery, setSearchQuery] = useState(initialSearchQuery)
   const history = useHistory()
-  const executeSearch = () => {
+  const executeSearch = useMemo(() => {
     history.push(`/search/${searchQuery}`)
-  }
+  }, [history, searchQuery])
   return { searchQuery, setSearchQuery, executeSearch }
 }
 
