eb6fc4c46bede37c1323dfe84d08658ccb608315
ekosenkosoft <e.kosenko@softteco.com>
2020-08-07 11:49:01 +0300

    Hook: Service hooks changed

--- src/@hooks/useServices/index.js
+++ src/@hooks/useServices/index.js
@@ -1,10 +1,8 @@
-import useCommonStoreon from './useCommonStoreon'
 import useFetchOnce from './useFetchOnce'
 import useFetchPerMount from './useFetchPerMount'
 
 const useServices = () => {
   return {
-    useCommonStoreon,
     useFetchOnce,
     useFetchPerMount,
   }
