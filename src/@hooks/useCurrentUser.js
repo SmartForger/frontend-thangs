8675c40b18176b7bb1c8ec3a64270ad6461c4ed5
Brandon Every <brandonevery@gmail.com>
2020-08-14 09:52:42 -0400

    Merge uploaded models with graphql returned ones

--- src/@hooks/useCurrentUser.js
+++ src/@hooks/useCurrentUser.js
@@ -1,14 +1,17 @@
 import * as GraphqlService from '@services/graphql-service'
 import { authenticationService } from '@services'
+import { useStoreon } from 'storeon/react'
 
 const graphqlService = GraphqlService.getInstance()
 
 const useCurrentUser = () => {
   const id = authenticationService.getCurrentUserId()
+  const { user: userStore } = useStoreon('user')
   const { loading, error, user } = graphqlService.useUserById(id)
   if (!id) {
     return { loading: false, error: undefined, user: undefined }
   }
+  if (user) user.models = [...userStore.models, ...user.models] //TEMP - This is to merge the user models cached by graphQL and new models uploaded as new versions - BE
   return { loading, error, user }
 }
 
