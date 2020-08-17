f966be3f5fff122694e3b6d3f3a45753aa7f8925
Brandon Every <brandonevery@gmail.com>
2020-08-17 17:24:32 -0400

    Fix lint

--- src/@components/ModelCard/index.jsx
+++ src/@components/ModelCard/index.jsx
@@ -208,14 +208,14 @@ const CardContents = ({
   const thumbnailUrl = model.fullThumbnailUrl
     ? model.fullThumbnailUrl
     : model.thumbnailUrl
-    ? model.thumbnailUrl
-    : `${THUMBNAILS_HOST}/${getThumbnailUrl(model)}`
+      ? model.thumbnailUrl
+      : `${THUMBNAILS_HOST}/${getThumbnailUrl(model)}`
 
   const waldoThumbnailUrl = searchModelFileName
     ? `${TIW_THUMBNAILS_HOST}/${getThumbnailFileName(model)}/${getWaldoThumbnailUrl(
-        model,
-        searchModelFileName
-      )}`
+      model,
+      searchModelFileName
+    )}`
     : undefined
 
   return (
