832501a151a1cdc01bb70379ce365de11dc668c0
Brandon Every <brandonevery@gmail.com>
2020-08-17 09:27:00 -0400

    1.2.1

--- src/@components/UploadProgress.jsx
+++ src/@components/UploadProgress.jsx
@@ -1,10 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { ProgressText, UploadFrame } from '@components'
-import { ReactComponent as CheckUploadingIcon } from '@svg/check-uploading-icon.svg'
-import { ReactComponent as LensUploadingIcon } from '@svg/lens-uploading-icon.svg'
-import { ReactComponent as GraphUploadingIcon } from '@svg/graph-uploading-icon.svg'
-import { ReactComponent as RulerUploadingIcon } from '@svg/ruler-uploading-icon.svg'
-import { ReactComponent as ProtractorUploadingIcon } from '@svg/protractor-uploading-icon.svg'
 import { createUseStyles } from '@style'
 
 const useStyles = createUseStyles(theme => {
@@ -53,48 +48,6 @@ const useStyles = createUseStyles(theme => {
     },
   }
 })
-
-const CHECK = 'check'
-const LENS = 'lens'
-const GRAPH = 'graph'
-const RULER = 'ruler'
-const PROTRACTOR = 'protractor'
-
-const Icons = ({ className }) => {
-  const [icon, setIcon] = useState(CHECK)
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (icon === CHECK) {
-        setIcon(LENS)
-      } else if (icon === LENS) {
-        setIcon(GRAPH)
-      } else if (icon === GRAPH) {
-        setIcon(RULER)
-      } else if (icon === RULER) {
-        setIcon(PROTRACTOR)
-      } else if (icon === PROTRACTOR) {
-        setIcon(LENS)
-      }
-    }, 2000)
-    return () => clearTimeout(timeout)
-  }, [icon, setIcon])
-
-  return (
-    <div className={className}>
-      {icon === CHECK ? (
-        <CheckUploadingIcon />
-      ) : icon === LENS ? (
-        <LensUploadingIcon />
-      ) : icon === GRAPH ? (
-        <GraphUploadingIcon />
-      ) : icon === RULER ? (
-        <RulerUploadingIcon />
-      ) : (
-        <ProtractorUploadingIcon />
-      )}
-    </div>
-  )
-}
 
 const UploadProgress = () => {
   const c = useStyles()
