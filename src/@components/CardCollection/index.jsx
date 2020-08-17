a5238274b2c4fc2e06acefea45ce62564d42d6dc
ekosenkosoft <e.kosenko@softteco.com>
2020-08-17 17:02:49 +0300

    Fixes: Single model card with issue fixed

--- src/@components/CardCollection/index.jsx
+++ src/@components/CardCollection/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { Children } from 'react'
 import { NoResults } from '@components'
 import Skeleton from '@material-ui/lab/Skeleton'
+import classnames from 'classnames'
 import { createUseStyles } from '@style'
-import Grid from './Grid'
 
 const useStyles = createUseStyles(_theme => {
   return {
+    CardCollection: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(21.5rem, 1fr))',
+      gap: '1rem',
+      width: '100%',
+    },
+    CardCollection__singleRow: {
+      gridTemplateColumns: 'repeat(auto-fill, 21.5rem)',
+    },
     ModelCard_Skeleton: {
       paddingBottom: 0,
       minHeight: '16.375rem',
@@ -21,7 +30,7 @@ const CardCollection = ({ maxPerRow = 4, noResultsText, loading = false, childre
 
   if (loading) {
     return (
-      <Grid singleRow={false}>
+      <div className={c.CardCollection}>
         {[...Array(12).keys()].map(index => (
           <Skeleton
             variant='rect'
@@ -29,12 +38,20 @@ const CardCollection = ({ maxPerRow = 4, noResultsText, loading = false, childre
             key={`skeletonCard:${index}`}
           />
         ))}
-      </Grid>
+      </div>
     )
   }
 
   if (children) {
-    return <Grid singleRow={children.length < maxPerRow}>{children}</Grid>
+    return (
+      <div
+        className={classnames(c.CardCollection, {
+          [c.CardCollection__singleRow]: Children.count(children) < maxPerRow,
+        })}
+      >
+        {children}
+      </div>
+    )
   } else {
     return <NoResults>{noResultsText}</NoResults>
   }
