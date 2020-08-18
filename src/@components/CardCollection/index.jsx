9037213b2aa9511d76b58c2f533634a7a6851e7a
Brandon Every <brandonevery@gmail.com>
2020-08-18 11:38:31 -0400

    fix lint

--- src/@components/CardCollection/index.jsx
+++ src/@components/CardCollection/index.jsx
@@ -45,9 +45,9 @@ const CardCollection = ({ maxPerRow = 4, noResultsText, loading = false, childre
   if (children) {
     const allItemsCount = Array.isArray(children)
       ? children.reduce(
-          (acc, child) => (child.props.items && acc + child.props.items.length) || 0,
-          0
-        )
+        (acc, child) => (child.props.items && acc + child.props.items.length) || 0,
+        0
+      )
       : (children.props.items && children.props.items.length) || 0
 
     return (
