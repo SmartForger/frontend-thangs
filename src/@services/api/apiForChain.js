6cfe3bea0ca6832f278217dbedc3d549ac8c8e93
ekosenkosoft <e.kosenko@softteco.com>
2020-08-20 18:27:28 +0300

    Fixes: Chain Api promise 403 fallback

--- src/@services/api/apiForChain.js
+++ src/@services/api/apiForChain.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { authenticationService } from '@services'
 
 export default ({ method = 'GET', endpoint, body, cancelToken, timeout = 300000 }) => {
   const token = localStorage.getItem('restAccessToken')
@@ -13,5 +14,12 @@ export default ({ method = 'GET', endpoint, body, cancelToken, timeout = 300000 
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
     ...(body && { data: JSON.stringify(body) }),
+  }).catch(e => {
+    if (e && e.response && e.response.status === 403) {
+      authenticationService.logout()
+      window.location.href = '/login?sessionExpired=true'
+    }
+
+    return Promise.reject(e)
   })
 }
--- src/@services/api/index.js
+++ src/@services/api/index.js
@@ -1,12 +1,7 @@
-import { authenticationService } from '@services'
 import apiForChain from './apiForChain'
 
 export default props =>
   apiForChain(props)
     .catch(e => {
-      if (e && e.response && e.response.status === 403) {
-        authenticationService.logout()
-        window.location.href = '/login?sessionExpired=true'
-      }
       return Promise.resolve({ data: {}, error: e })
     })
