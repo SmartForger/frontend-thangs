d9a6d9e25d23f11ef4f2d2e470e9219c0cf00b64
Brandon Every <brandonevery@gmail.com>
2020-08-21 12:57:18 -0400

    Compress folder cards

--- src/@components/FolderCard/index.jsx
+++ src/@components/FolderCard/index.jsx
@@ -1,71 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Card, ModelThumbnail, FolderInfo } from '@components'
+import { Card, FolderInfo } from '@components'
 import classnames from 'classnames'
 import { createUseStyles } from '@style'
 
-const useStyles = createUseStyles(theme => {
+const useStyles = createUseStyles(_theme => {
   return {
     FolderCard: {},
-    FolderCard_Grid: {
-      display: 'grid',
-      height: '100%',
-      gridTemplateAreas: `
-            'info info'
-            'model-one model-two'`,
-      gridTemplateRows: 'repeat(2, minmax(50%, 140px))',
-      gridGap: '.25rem',
-    },
-    FolderCard_Placeholder: {
-      backgroundColor: theme.colors.GREY_13,
-    },
-    FolderCard_ModelThumbnail: {
-      margin: 'auto',
-      '& img': {
-        maxWidth: '100%',
-        maxHeight: '100%',
-      },
-    },
     FolderCard_CardContents: {
       gridArea: 'info',
       borderRadius: '.5rem .5rem 0 0',
-    },
-    FolderCard_ModelOne: {
-      gridArea: 'model-one',
-    },
-    FolderCard_ModelTwo: {
-      gridArea: 'model-two',
+      height: 'auto !important',
     },
   }
 })
 
-const Placeholder = () => {
-  const c = useStyles()
-  return <Card className={c.FolderCard_Placeholder} />
-}
-
-const PossiblyEmptyModelCard = ({ model, className }) => {
-  const c = useStyles()
-  return model ? (
-    <Card className={className}>
-      <ModelThumbnail className={c.FolderCard_ModelThumbnail} {...model} />
-    </Card>
-  ) : (
-    <Placeholder className={className} />
-  )
-}
-
 const CardContents = ({ className, folder }) => {
   const c = useStyles()
-  const [model1, model2] = folder.models
   return (
-    <div className={classnames(className, c.FolderCard_Grid)}>
-      <Card className={c.FolderCard_CardContents}>
-        <FolderInfo name={folder.name} members={folder.members} models={folder.models} />
-      </Card>
-      <PossiblyEmptyModelCard className={c.FolderCard_ModelOne} model={model1} />
-      <PossiblyEmptyModelCard className={c.FolderCard_ModelTwo} model={model2} />
-    </div>
+    <Card className={classnames(className, c.FolderCard_CardContents)}>
+      <FolderInfo name={folder.name} members={folder.members} models={folder.models} />
+    </Card>
   )
 }
 
