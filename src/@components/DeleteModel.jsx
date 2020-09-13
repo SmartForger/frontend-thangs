e024b79044069f7f0fd0291af7a143bdc1c753c9
Brandon Every <brandonevery@gmail.com>
2020-09-13 19:50:55 -0400

    Fix css mixin

--- src/@components/DeleteModel.jsx
+++ src/@components/DeleteModel.jsx
@@ -28,7 +28,7 @@ const useStyles = createUseStyles(theme => {
       backgroundColor: theme.colors.white[100],
       boxShadow: '0px 5px 10px 0px rgba(35, 37, 48, 0.25)',
       borderRadius: '0.5rem',
-      ...theme.mixins.text.viewerLoadingText,
+      ...theme.text.viewerLoadingText,
       padding: '1rem 1rem',
 
       position: 'absolute',
