a4a9718b48ee82a50d498262dfcbd8ecd2290179
Brandon Every <brandonevery@gmail.com>
2020-09-14 17:40:20 -0400

    Add signup overlay to unauthed follow button on profile

--- src/@components/ProfileButton.jsx
+++ src/@components/ProfileButton.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Link } from 'react-router-dom'
+import { useStoreon } from 'storeon/react'
 import classnames from 'classnames'
 import { createUseStyles } from '@style'
 import { useCurrentUserId } from '@hooks'
 import { ToggleFollowButton } from '@components'
+import * as types from '@constants/storeEventTypes'
 
 const useStyles = createUseStyles(theme => {
   return {
@@ -18,8 +20,21 @@ const useStyles = createUseStyles(theme => {
 const ProfileButton = ({ userId, className }) => {
   const c = useStyles()
   const currentUserId = useCurrentUserId()
+  const { dispatch } = useStoreon()
   const isCurrentUser = currentUserId === userId
-
+  const openSignupOverlay = useCallback(
+    titleMessage => {
+      dispatch(types.OPEN_OVERLAY, {
+        overlayName: 'signUp',
+        overlayData: {
+          animateIn: true,
+          windowed: true,
+          titleMessage,
+        },
+      })
+    },
+    [dispatch]
+  )
   if (isCurrentUser) {
     return (
       <Link className={classnames(className, c.ProfileButton)} to='/profile/edit'>
@@ -27,7 +42,13 @@ const ProfileButton = ({ userId, className }) => {
       </Link>
     )
   } else {
-    return <ToggleFollowButton profileUserId={userId} currentUser={currentUserId} />
+    return (
+      <ToggleFollowButton
+        profileUserId={userId}
+        currentUser={currentUserId}
+        openSignupOverlay={openSignupOverlay}
+      />
+    )
   }
 }
 
