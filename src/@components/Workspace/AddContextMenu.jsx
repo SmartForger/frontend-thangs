bf5b4d6c3ed6456dbf7a8e39ef440777b5349900
Brandon Every <brandonevery@gmail.com>
2020-10-18 13:16:16 -0400

    Remove overactive track event

--- src/@components/Workspace/AddContextMenu.jsx
+++ src/@components/Workspace/AddContextMenu.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { ContextMenu } from 'react-contextmenu'
 import { AddMenu } from '@components'
-import { track } from '@utilities/analytics'
 
 const AddContextMenu = ({ folder }) => {
-  track('Add Context Menu Open', { folder })
   return (
     <ContextMenu id='Add_Menu'>
       <AddMenu folder={folder} />
--- src/@components/Workspace/FileContextMenu.jsx
+++ src/@components/Workspace/FileContextMenu.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ContextMenu } from 'react-contextmenu'
 import { FileMenu } from '@components'
 import { createUseStyles } from '@style'
-import { track } from '@utilities/analytics'
 
 const useStyles = createUseStyles(_theme => {
   return {
@@ -14,7 +13,6 @@ const useStyles = createUseStyles(_theme => {
 
 const FileContextMenu = ({ folder, model, id, postId = '', type }) => {
   const c = useStyles({})
-  track('File Context Menu Open', { type })
   return (
     <ContextMenu className={c.ContextMenu} id={`File_Menu_${id}${postId}`}>
       <FileMenu folder={folder} model={model} id={id} type={type} />
