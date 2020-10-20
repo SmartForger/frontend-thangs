32faf06c77991e0cdb0c8846058e4319f19274f1
ekosenkosoft <e.kosenko@softteco.com>
2020-10-20 15:02:04 +0300

    Autotests: Model test: timeout changed

--- cypress/integration/portal/landing/landing_spec.js
+++ cypress/integration/portal/landing/landing_spec.js
@@ -48,7 +48,7 @@ describe('The Landing Page', () => {
     inputFocus(CLASSES.HEADER_DESKTOP, searchInput)
     isElement(CLASSES.LANDING_SEARCH_BAR_UPLOAD, PROPS.VISIBLE)
     uploadFile(MODEL.FILENAME, uploadInput)
-    urlShouldIncludeAfterTimeout(MODEL.FILENAME, 60000)
+    urlShouldIncludeAfterTimeout(MODEL.FILENAME, 180000)
     urlShouldInclude('modelId')
     urlShouldInclude('phynId')
   })
