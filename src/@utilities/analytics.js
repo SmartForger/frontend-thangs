454b2fa041e2f51119e0ec70ee65aef55428aa3b
Brandon Every <brandonevery@gmail.com>
2020-10-21 08:49:50 -0400

    Update logger for amplitude

--- src/@utilities/analytics.js
+++ src/@utilities/analytics.js
@@ -5,7 +5,7 @@ import { logger } from '@utilities/logging'
 export const initialize = () => {
   const amplitudeKey = process.env.REACT_APP_AMPLITUDE_ID
   if (amplitudeKey === undefined) {
-    logger.warn('Missing Pendo API key')
+    logger.warn('Missing Amplitude API key')
   }
   amplitude.getInstance().init(amplitudeKey)
   pendo.initialize()
