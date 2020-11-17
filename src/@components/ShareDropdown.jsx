a269f48c979958f9aeb5fa71e912d0e601404338
Brandon Every <brandonevery@gmail.com>
2020-11-16 22:32:51 -0500

    revert share url back to thangs

--- src/@components/ShareDropdown.jsx
+++ src/@components/ShareDropdown.jsx
@@ -41,7 +41,7 @@ const useStyles = createUseStyles(theme => {
 const noop = () => null
 export const ShareDropdownMenu = ({ TargetComponent, iconOnly, title }) => {
   const c = useStyles({})
-  const shareUrl = `${process.env.REACT_APP_API_KEY}/social/${window.location.pathname}`
+  const shareUrl = `${window.location.origin}/social/${window.location.pathname}`
   return (
     <DropdownMenu
       className={c.ShareDropdown}
