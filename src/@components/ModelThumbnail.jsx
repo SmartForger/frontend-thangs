03048ba1feaa9e465c489eeddc4f035b1e7327b9
Brandon Every <brandonevery@gmail.com>
2020-12-03 10:32:38 -0500

    Remove size param and remove optimizely block from landing page

--- src/@components/ModelThumbnail.jsx
+++ src/@components/ModelThumbnail.jsx
@@ -106,7 +106,7 @@ const thumbnailUrl = model =>
     ? model.fullThumbnailUrl
     : model.thumbnailUrl
       ? model.thumbnailUrl
-      : `${THUMBNAILS_HOST}/${getThumbnailUrl(model)}?size=456x540`
+      : `${THUMBNAILS_HOST}/${getThumbnailUrl(model)}`
 
 const getThumbnailUrl = (model = {}) => {
   if (model.thumbnailUrl) return model.thumbnailUrl
