3a41a50aeb75db93dc9f257e5eeb3122d4146570
ekosenkosoft <e.kosenko@softteco.com>
2020-12-18 16:29:40 +0300

    Redundant code from EditModelForm deleted

--- src/@components/EditModelForm.jsx
+++ src/@components/EditModelForm.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState, useRef } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import * as R from 'ramda'
 import {
   Button,
@@ -8,7 +8,6 @@ import {
   Textarea,
   TitleTertiary,
   Spacer,
-  MultiLineBodyText,
 } from '@components'
 import { createUseStyles } from '@style'
 import { useForm } from '@hooks'
@@ -108,12 +107,10 @@ const CATEGORIES = [
 
 const EditModelForm = ({
   model = {},
-  handleSubmit = noop,
-  handleDelete = noop,
+  onSubmit = noop,
   editProfileErrorMessage,
 }) => {
   const c = useStyles()
-  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false)
   const firstInputRef = useRef(null)
 
   const initialState = {
@@ -137,14 +134,6 @@ const EditModelForm = ({
     [onInputChange]
   )
 
-  const handleCancelDelete = useCallback(() => {
-    setShowDeleteConfirm(false)
-  }, [])
-
-  const handleConfirmDelete = useCallback(() => {
-    handleDelete()
-  }, [handleDelete])
-
   const selectedCategory = useMemo(() => {
     return R.find(R.propEq('value', model.category), CATEGORIES)
   }, [model])
@@ -155,119 +144,97 @@ const EditModelForm = ({
 
   return (
     <div className={c.EditModelForm_Wrapper}>
-      {!showDeleteConfirm && (
-        <>
-          <Spacer size='4rem' />
-          <div className={c.EditModelForm_ModelTitle}>
-            <FileIcon />
-            <Spacer size={'1rem'} />
-            <TitleTertiary title={model.name}>{model.name}</TitleTertiary>
-            <Spacer size={'.5rem'} />
-            <LikeModelButton className={c.FileCard_Star} model={model} minimal />
-          </div>
+      <>
+        <Spacer size='4rem' />
+        <div className={c.EditModelForm_ModelTitle}>
+          <FileIcon />
+          <Spacer size={'1rem'} />
+          <TitleTertiary title={model.name}>{model.name}</TitleTertiary>
+          <Spacer size={'.5rem'} />
+          <LikeModelButton className={c.FileCard_Star} model={model} minimal />
+        </div>
+        <Spacer size='2rem' />
+        <form
+          className={c.EditModelForm}
+          onSubmit={onFormSubmit(onSubmit)}
+          data-cy='edit-model-form'
+        >
+          {editProfileErrorMessage && (
+            <>
+              <h4 className={c.EditModel_ErrorText} data-cy='edit-model-error'>
+                {editProfileErrorMessage}
+              </h4>
+              <Spacer size='1rem' />
+            </>
+          )}
+          <Input
+            id='name-input'
+            name='name'
+            label='Name'
+            maxLength='150'
+            value={inputState && inputState.name}
+            onChange={handleOnInputChange}
+            inputRef={firstInputRef}
+            required
+          />
+          <Spacer size='1rem' />
+          <Textarea
+            id='description-input'
+            name='description'
+            label='Description'
+            type='description'
+            value={inputState && inputState.description}
+            onChange={handleOnInputChange}
+            required
+          />
+          <Spacer size='1rem' />
+          <Input
+            id='material-input'
+            name='material'
+            label='Material'
+            maxLength='150'
+            type='material'
+            value={inputState && inputState.material}
+            onChange={handleOnInputChange}
+          />
+          <Spacer size='1rem' />
+          <Input
+            id='height-input'
+            name='height'
+            label='Height'
+            maxLength='150'
+            type='height'
+            value={inputState && inputState.height}
+            onChange={handleOnInputChange}
+          />
+          <Spacer size='1rem' />
+          <Input
+            id='weight-input'
+            name='weight'
+            label='Weight'
+            maxLength='150'
+            type='weight'
+            value={inputState && inputState.weight}
+            onChange={handleOnInputChange}
+          />
+          <Spacer size='1rem' />
+          <Dropdown
+            className={c.UploadForm_Select}
+            name='category'
+            placeholder='Select category'
+            defaultValue={selectedCategory}
+            isClearable
+            options={CATEGORIES}
+            onChange={e => {
+              if (e) handleOnInputChange('category', e.value)
+            }}
+          />
           <Spacer size='2rem' />
-          <form
-            className={c.EditModelForm}
-            onSubmit={onFormSubmit(handleSubmit)}
-            data-cy='edit-model-form'
-          >
-            {editProfileErrorMessage && (
-              <>
-                <h4 className={c.EditModel_ErrorText} data-cy='edit-model-error'>
-                  {editProfileErrorMessage}
-                </h4>
-                <Spacer size='1rem' />
-              </>
-            )}
-            <Input
-              id='name-input'
-              name='name'
-              label='Name'
-              maxLength='150'
-              value={inputState && inputState.name}
-              onChange={handleOnInputChange}
-              inputRef={firstInputRef}
-              required
-            />
-            <Spacer size='1rem' />
-            <Textarea
-              id='description-input'
-              name='description'
-              label='Description'
-              type='description'
-              value={inputState && inputState.description}
-              onChange={handleOnInputChange}
-              required
-            />
-            <Spacer size='1rem' />
-            <Input
-              id='material-input'
-              name='material'
-              label='Material'
-              maxLength='150'
-              type='material'
-              value={inputState && inputState.material}
-              onChange={handleOnInputChange}
-            />
-            <Spacer size='1rem' />
-            <Input
-              id='height-input'
-              name='height'
-              label='Height'
-              maxLength='150'
-              type='height'
-              value={inputState && inputState.height}
-              onChange={handleOnInputChange}
-            />
-            <Spacer size='1rem' />
-            <Input
-              id='weight-input'
-              name='weight'
-              label='Weight'
-              maxLength='150'
-              type='weight'
-              value={inputState && inputState.weight}
-              onChange={handleOnInputChange}
-            />
-            <Spacer size='1rem' />
-            <Dropdown
-              className={c.UploadForm_Select}
-              name='category'
-              placeholder='Select category'
-              defaultValue={selectedCategory}
-              isClearable
-              options={CATEGORIES}
-              onChange={e => {
-                if (e) handleOnInputChange('category', e.value)
-              }}
-            />
-            <Spacer size='2rem' />
-            <Button type='submit'>Save Changes</Button>
-            <Spacer size='1rem' />
-          </form>
-          <Spacer size='2.5rem' />
-        </>
-      )}
-      {showDeleteConfirm && (
-        <>
-          <TitleTertiary>Delete Model</TitleTertiary>
-          <Spacer size='2rem' />
-          <div>
-            <MultiLineBodyText>
-              Are you sure you want to delete{' '}
-              <b className={c.EditModel_ModelName}>{model.name}</b>?
-            </MultiLineBodyText>
-            <Spacer className={c.EditModel_MobileSpacer} size='2rem' />
-            <div className={c.EditModelForm_ButtonRow}>
-              <Button secondary onClick={handleCancelDelete}>
-                Cancel
-              </Button>
-              <Spacer className={c.EditModel_MobileSpacer} size='1rem' />
-              <Button onClick={handleConfirmDelete}>Confirm</Button>
-            </div>
-          </div>
-        </>
-      )}
+          <Button type='submit'>Save Changes</Button>
+          <Spacer size='1rem' />
+        </form>
+        <Spacer size='2.5rem' />
+      </>
     </div>
   )
 }
