eb93bc4e6ea8c081343167893c1d6a72a87493c8
Brandon Every <brandonevery@gmail.com>
2021-01-11 11:23:10 -0500

    Update slider to design spec

--- src/@components/Slider.jsx
+++ src/@components/Slider.jsx
@@ -1,58 +1,65 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { default as MuiSlider } from '@material-ui/core/Slider'
+import { Spacer } from '@components'
 
 const CustomSlider = withStyles({
   root: {
-    width: '7.25rem',
     color: '#242424',
     height: '.25rem',
     padding: '13px 0',
+    width: '7.25rem',
   },
   thumb: {
-    height: '1.5rem',
-    width: '1.5rem',
-    borderRadius: '.5rem',
+    alignItems: 'center',
     backgroundColor: '#fff',
     border: '2px solid currentColor',
-    marginTop: -11,
+    borderRadius: '.5rem',
+    display: 'flex',
+    flexDirection: 'row',
+    height: '1.5rem',
+    justifyContent: 'center',
     marginLeft: -24,
+    marginTop: -11,
     transform: 'translateX(.75rem)',
+    width: '1.5rem',
 
     '&:hover': {
       boxShadow: 'none',
     },
     '& .bar': {
-      height: 9,
-      width: 1,
       backgroundColor: 'currentColor',
-      marginLeft: 1,
-      marginRight: 1,
+      borderRadius: 2,
+      height: 10,
+      width: 2,
     },
   },
   active: {
     boxShadow: '0px 0px 5px 5px rgba(36, 36, 36, 0.16)',
   },
   focusVisible: {
-    boxShadow: '0px 0px 5px 5px rgba(36, 36, 36, 0.16) !important',
+    boxShadow: 'none',
   },
   track: {
-    height: 3,
+    borderRadius: 4,
+    height: 4,
   },
-  // rail: {
-  //   color: '#999',
-  //   opacity: 1,
-  //   height: 3,
-  //   padding: '0 10px',
-  // },
+  rail: {
+    borderRadius: 4,
+    height: 4,
+  },
 })(MuiSlider)
 
 function CustomThumbComponent(props) {
   return (
     <span {...props}>
+      <Spacer size={'5px'} />
       <span className='bar' />
+      <Spacer size={'2px'} />
       <span className='bar' />
+      <Spacer size={'2px'} />
       <span className='bar' />
+      <Spacer size={'5px'} />
     </span>
   )
 }
