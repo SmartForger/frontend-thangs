8251773e56de9eaab1c8f042d3327795280906f9
Brandon Every <brandonevery@gmail.com>
2021-02-04 16:04:42 -0500

    fix lint

--- src/@overlays/MultiUpload/index.jsx
+++ src/@overlays/MultiUpload/index.jsx
@@ -174,8 +174,8 @@ const MultiUpload = ({ initData = null, folderId = '' }) => {
           ? newNode.subIds.map(subId => formNode(subId))
           : []
         : Object.values(newTreeData)
-            .filter(node => !node.parentId)
-            .map(node => formNode(node.id))
+          .filter(node => !node.parentId)
+          .map(node => formNode(node.id))
 
       newNode.treeValid =
         newNode.valid &&
@@ -470,10 +470,10 @@ const MultiUpload = ({ initData = null, folderId = '' }) => {
               {!activeNode
                 ? 'Upload Files'
                 : activeNode.isAssembly && activeNode.parentId
-                ? 'Sub Assembly'
-                : activeNode.isAssembly
-                ? 'New Assembly'
-                : partFormTitle}
+                  ? 'Sub Assembly'
+                  : activeNode.isAssembly
+                    ? 'New Assembly'
+                    : partFormTitle}
             </SingleLineBodyText>
             {activeView > -1 && (
               <ArrowLeftIcon className={c.MultiUpload_BackButton} onClick={handleBack} />
