d9ca33e61917b3c3a9f11269e76fa15827500dc5
Evgeny Kosenko <e.kosenko@softteco.com>
2021-02-08 16:13:49 +0300

    Autotests: follow refactored

--- cypress/integration/follows_spec.js
+++ cypress/integration/follows_spec.js
@@ -1,94 +1,38 @@
 import {
+  clearModelsAndFolders,
   goTo,
   isTextInsideClass,
   loginByUser,
+  unfollowUser,
 } from '../utils/common-methods'
-import { CLASSES, MODEL_CARD, PROPS, TEXT, USER, USER2, USER3, USER4 } from '../utils/constants'
+import { CLASSES, MODEL_CARD, PROPS, TEXT, USER3, USER4 } from '../utils/constants'
 import { multiUpload } from '../utils/uploadMethods'
-import api, { apiLogin } from '../utils/api'
+
+const ACTION_USER1 = USER3
+const SUPPORT_USER2 = USER4
 
 describe('User notifications', () => {
   before(() => {
-    // BE-reset: Test2 unfollows Test if it needs
-    apiLogin({ userName: USER4.EMAIL, password: USER4.PASSWORD })
-      .then(() => {
-        return api({
-          endpoint: `users/${USER3.ID}`,
-          method: 'GET',
-        })
-      })
-
-      .then(response => {
-        const user = response.body || {}
-        const isFollowed = user.isBeingFollowedByRequester
-
-        cy.log('########################### Is followed', isFollowed)
-        if (isFollowed) {
-          return api({
-            endpoint: `users/${USER3.ID}/unfollow`,
-            method: 'DELETE',
-          })
-        } else {
-          return Promise.resolve('ok')
-        }
-      })
-      .then(response => {
-        cy.log(
-          '########################### Finish',
-          response === 'End' && JSON.stringify(response)
-        )
-      })
-
-    // BE-reset: Test1 delete all the models
-    apiLogin({ userName: USER3.EMAIL, password: USER3.PASSWORD })
-      .then(() => {
-        return api({
-          endpoint: `users/${USER3.ID}/thangs`,
-          method: 'GET',
-        })
-      })
-
-      .then(response => {
-        const data = response.body || {}
-        const models = data.models || []
-
-        cy.log('########################### User models', models.length)
-
-        const deleteModelRec = async modelsIds => {
-          if (modelsIds.length > 0) {
-            const modelId = modelsIds.pop()
-
-            await api({
-              endpoint: `models/${modelId}`,
-              method: 'DELETE',
-            })
-
-            cy.log('########################### Model deleted', modelId)
-
-            deleteModelRec(modelsIds)
-          }
-        }
-
-        deleteModelRec(models.map(model => model.id))
-      })
+    unfollowUser(SUPPORT_USER2, ACTION_USER1)
+    clearModelsAndFolders(ACTION_USER1)
   })
 
   it('User1 uploads model', () => {
     localStorage.clear()
 
     loginByUser({
-      email: USER3.EMAIL,
-      password: USER3.PASSWORD,
+      email: ACTION_USER1.EMAIL,
+      password: ACTION_USER1.PASSWORD,
     })
     multiUpload()
   })
 
   it('User2 follows and unfollows User1 from public portfolio', () => {
     loginByUser({
-      email: USER4.EMAIL,
-      password: USER4.PASSWORD,
+      email: SUPPORT_USER2.EMAIL,
+      password: SUPPORT_USER2.PASSWORD,
     })
-    goTo(`/${USER3.NAME}`)
+    goTo(`/${ACTION_USER1.NAME}`)
 
     isTextInsideClass(CLASSES.PROFILE_FOLLOW_BUTTON, TEXT.FOLLOW, PROPS.VISIBLE)
     cy.get(CLASSES.PROFILE_FOLLOW_BUTTON).click()
@@ -100,10 +44,10 @@ describe('User notifications', () => {
 
   it('User2 follows and unfollows User1 from model', () => {
     loginByUser({
-      email: USER4.EMAIL,
-      password: USER4.PASSWORD,
+      email: SUPPORT_USER2.EMAIL,
+      password: SUPPORT_USER2.PASSWORD,
     })
-    goTo(`/${USER3.NAME}`)
+    goTo(`/${ACTION_USER1.NAME}`)
     cy.wait(3000)
     cy.get(MODEL_CARD()).click()
 
