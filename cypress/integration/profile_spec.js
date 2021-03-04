9228637a51e93a042979187c3192971b6b12a682
Evgeny Kosenko <e.kosenko@softteco.com>
2021-03-04 14:46:48 +0300

    Autotests: profile_spec updated

--- cypress/integration/profile_spec.js
+++ cypress/integration/profile_spec.js
@@ -2,19 +2,67 @@ import {
   goTo,
   isTextInsideClass,
   loginByUser,
+  isElementContains,
+  clearModelsAndFolders,
 } from '../utils/common-methods'
+import { multiUpload } from '../utils/uploadMethods'
 
 const FIRST_NAME_TEXT = 'FirstTestName'
 const LAST_NAME_TEXT = 'LastTestName'
 const DESCRIPTION_TEXT = 'Bio Here'
 
-describe('Profile', () => {
-  let activeUser
+let activeUser
+let sideUser
 
+describe('Profile', () => {
   before(() => {
     cy.getCookie('activeUser').then(({ value }) => {
       activeUser = JSON.parse(value)
     })
+    cy.getCookie('sideUser').then(({ value }) => {
+      sideUser = JSON.parse(value)
+    })
+  })
+
+  after(() => {
+    clearModelsAndFolders(activeUser)
+  })
+
+  it('Side user upploads model', () => {
+    loginByUser({
+      email: sideUser.EMAIL,
+      password: sideUser.PASSWORD,
+    })
+    multiUpload()
+  })
+
+  it('Active user likes', () => {
+    loginByUser({
+      email: activeUser.EMAIL,
+      password: activeUser.PASSWORD,
+    })
+    goTo(`/${sideUser.NAME}`)
+
+    cy.intercept('/like').as('likeRequest')
+    cy.get('[class^=ModelCard_ActivityCount][title=Like]').click()
+
+    cy.wait('@likeRequest').its('response.statusCode').should('eq', 200)
+  })
+
+  it('Active user redirects to other portfolio', () => {
+    loginByUser({
+      email: activeUser.EMAIL,
+      password: activeUser.PASSWORD,
+    })
+    goTo(`/${activeUser.NAME}`)
+
+    cy.get('[class^=Home_Row] [class^=BodyText]')
+      .contains('Likes', { timeout: 10000 })
+      .click()
+
+    cy.get('[class^=ModelCard_UserLine]', { timeout: 10000 }).click()
+
+    isElementContains('[class^=Profile_Header] [class^=Profile_Name]', sideUser.NAME)
   })
 
   it('User data updates after edit', () => {
