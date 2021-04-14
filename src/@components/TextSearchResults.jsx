43724bb6635252e643a464b3868e33392cf3133b
Brandon Every <brandonevery@gmail.com>
2021-04-14 14:31:14 -0400

    Adjust spacer for toolbar

--- src/@components/TextSearchResults.jsx
+++ src/@components/TextSearchResults.jsx
@@ -311,7 +311,7 @@ const ModelDetails = ({
           />
         </>
       )}
-      <Spacer size={'1.5rem'} />
+      <Spacer size={'1rem'} />
       <SearchResultFooter model={model} onFindRelated={findRelated} />
     </div>
   )
