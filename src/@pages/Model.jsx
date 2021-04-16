c2b62c3da0584687eb01b6114b325746f1fe8bbb
Marcel Kooi <mkooi@physna.com>
2021-04-16 11:17:51 -0400

    v1.30.27

--- src/@pages/Model.jsx
+++ src/@pages/Model.jsx
@@ -348,7 +348,6 @@ const DownloadARLink = ({ model, isAuthedUser, openSignupOverlay = noop }) => {
   const { dispatch } = useStoreon()
   const downloadModel = useCallback(
     format => {
-      if (format !== 'android') return //TEMP - Remove once ios is available
       dispatch(types.FETCH_MODEL_DOWNLOAD_URL, {
         id: model.id,
         format,
@@ -696,9 +695,8 @@ const ModelDetailPage = ({
     modelTitle = modelTitle.substring(0, titleCharCount)
   titleCharCount = titleCharCount - modelTitle.length
   const modelTitleAuthor = titleCharCount >= modelAuthor.length + 3 ? modelAuthor : ''
-  const pageTitle = `${modelTitle}${titlePrefix}|${
-    modelTitleAuthor ? ` ${modelTitleAuthor} |` : ''
-  }${titleSuffix}`
+  const pageTitle = `${modelTitle}${titlePrefix}|${modelTitleAuthor ? ` ${modelTitleAuthor} |` : ''
+    }${titleSuffix}`
   const modelDescription = modelData.description || defaultDescription
 
   return (
@@ -707,16 +705,14 @@ const ModelDetailPage = ({
         <title>{pageTitle}</title>
         <meta
           name='description'
-          content={`${descriptionPrefix}${
-            modelData.name
-          }, ${descriptionCreatedBy}${modelAuthor}. ${modelDescription.slice(0, 160)}`}
+          content={`${descriptionPrefix}${modelData.name
+            }, ${descriptionCreatedBy}${modelAuthor}. ${modelDescription.slice(0, 160)}`}
         />
         <meta property='og:title' content={pageTitle} />
         <meta
           property='og:description'
-          content={`${descriptionPrefix}${
-            modelData.name
-          }, ${descriptionCreatedBy}${modelAuthor}. ${modelDescription.slice(0, 160)}`}
+          content={`${descriptionPrefix}${modelData.name
+            }, ${descriptionCreatedBy}${modelAuthor}. ${modelDescription.slice(0, 160)}`}
         />
         <meta
           property='og:image'
