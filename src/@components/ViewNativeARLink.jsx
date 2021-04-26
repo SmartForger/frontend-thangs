b4eead9d23df500d64f38fcd860214afddee714c
Adam Gleichsner <agleichsner@physna.com>
2021-04-26 16:58:52 -0400

    First pass at on-demand glb conversion for android intent link

--- src/@components/ViewNativeARLink.jsx
+++ src/@components/ViewNativeARLink.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react'
+import * as R from 'ramda'
 import { useStoreon } from 'storeon/react'
 
 import { createUseStyles } from '@physna/voxel-ui/@style'
@@ -6,6 +7,7 @@ import { createUseStyles } from '@physna/voxel-ui/@style'
 import * as types from '@constants/storeEventTypes'
 import { NativeARDownloadActionMenu, Spacer } from '@components'
 import { track } from '@utilities/analytics'
+import axios from 'axios'
 
 const useStyles = createUseStyles(theme => {
   const {
@@ -44,16 +46,42 @@ const ViewNativeARLink = ({
       dispatch(types.FETCH_MODEL_DOWNLOAD_URL, {
         id: model.id ?? model.modelId,
         format,
-        onFinish: downloadUrl => {
+        onFinish: async downloadUrl => {
           track(trackingEvent, { format, modelId: model.id ?? model.modelId })
 
           if (format === 'android') {
-            const link = document.createElement('a')
-            link.href = `intent://arvr.google.com/scene-viewer/1.0?file=${downloadUrl.replaceAll(
-              '#',
-              encodeURIComponent('#')
-            )}&mode=ar_only#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
-            link.click()
+            const { parts } = model
+            let primaryPart = null
+            if (parts) {
+              if (parts.length > 1) {
+                primaryPart = R.find(R.propEq('isPrimary', true))(parts) || parts[0]
+              } else {
+                primaryPart = parts[0]
+              }
+            }
+
+            if (primaryPart && primaryPart.androidUrl) {
+              let isGLBReady = false
+              try {
+                const { error } = await axios.get(downloadUrl)
+                if (!error) {
+                  isGLBReady = true
+                }
+              } catch (e) {
+                if (e.isAxiosError) {
+                  isGLBReady = true
+                }
+              }
+
+              if (isGLBReady) {
+                const link = document.createElement('a')
+                link.href = `intent://arvr.google.com/scene-viewer/1.0?file=${downloadUrl.replaceAll(
+                  '#',
+                  encodeURIComponent('#')
+                )}&mode=ar_only#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
+                link.click()
+              }
+            }
           } else if (format === 'ios') {
             window.location.assign(
               `${downloadUrl.replaceAll(
