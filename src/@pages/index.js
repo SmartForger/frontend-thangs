a55e3d6724aedf0ab004b4f37db13fb812714a36
Carl von Buelow <cvbuelow@gmail.com>
2021-04-29 15:19:29 -0400

    try code splitting by route

--- src/@pages/index.js
+++ src/@pages/index.js
@@ -1,13 +1,26 @@
-export { default as AboutUs } from './AboutUs'
-export { default as Auth } from './Auth'
-export { default as Landing } from './Landing'
-export { default as ModelDetail } from './Model'
-export { default as MyThangs } from './MyThangs'
+import { lazy } from 'react'
+// export { default as AboutUs } from './AboutUs'
+// export { default as Auth } from './Auth'
+// export { default as Landing } from './Landing'
+// export { default as ModelDetail } from './Model'
+// export { default as MyThangs } from './MyThangs'
 export * from './PasswordReset'
-export { default as Profile } from './Profile'
-export { default as PrivacyPolicy } from './PrivacyPolicy'
-export { default as RedirectModel } from './RedirectModel'
-export { default as RedirectProfile } from './RedirectProfile'
-export { default as SearchResults } from './SearchResults'
-export { default as TermsAndConditions } from './TermsAndConditions'
+// export { default as Profile } from './Profile'
+// export { default as PrivacyPolicy } from './PrivacyPolicy'
+// export { default as RedirectModel } from './RedirectModel'
+// export { default as RedirectProfile } from './RedirectProfile'
+// export { default as SearchResults } from './SearchResults'
+// export { default as TermsAndConditions } from './TermsAndConditions'
 export * from './404'
+
+export const AboutUs = lazy(() => import('./AboutUs'))
+export const Auth = lazy(() => import('./Auth'))
+export const Landing = lazy(() => import('./Landing'))
+export const ModelDetail = lazy(() => import('./Model'))
+export const MyThangs = lazy(() => import('./MyThangs'))
+export const Profile = lazy(() => import('./Profile'))
+export const PrivacyPolicy = lazy(() => import('./PrivacyPolicy'))
+export const RedirectModel = lazy(() => import('./RedirectModel'))
+export const RedirectProfile = lazy(() => import('./RedirectProfile'))
+export const SearchResults = lazy(() => import('./SearchResults'))
+export const TermsAndConditions = lazy(() => import('./TermsAndConditions'))
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Router, Switch } from 'react-router-dom'
 import { history } from './history'
 import {
@@ -43,48 +43,50 @@ const App = () => {
           <ScrollToTop />
           <ActionMenuProvider>
             <OverlayProvider>
-              <Switch>
-                <Route exact path='/' render={props => <Landing {...props} />} />
-                <Route
-                  path='/authenticate/:provider'
-                  render={props => <Auth {...props} />}
-                />
-                <Route path='/explore/:id' component={Landing} />
-                <Route
-                  path='/welcome'
-                  render={props => <Landing {...props} newSignUp={true} />}
-                />
-                <Route
-                  path='/terms-and-conditions'
-                  exact
-                  component={TermsAndConditions}
-                />
-                <Route path='/privacy-policy' exact component={PrivacyPolicy} />
-                <Route path='/about-us' exact component={AboutUs} />
-                <Route path='/home' component={routeRedirectToProfile()} />
-                <Route exact path='/password-reset' component={PasswordReset} />
-                <Route
-                  path='/password_reset_confirm/:token'
-                  component={ConfirmPasswordReset}
-                />
-                <Route path='/mythangs' component={routeRequiresAuth(MyThangs)} />
-                <Route
-                  path={['/search/:searchQuery', '/search']}
-                  component={SearchResults}
-                />
-                <Route path='/profile/:id' component={RedirectProfile} />
-                <Route path='/u/:id' component={RedirectProfile} />
-                <Route path='/model/:modelId' component={RedirectModel} />
-                <Route path='/models/:modelId' component={RedirectModel} />
-                <Route path='/m/:modelId' component={RedirectModel} />
-                <Route
-                  path='/:userName/:modelString'
-                  render={props => <ModelDetail {...props} />}
-                />
-                <Route path='/:userName' component={Profile} />
-                <Route path='/404' component={Page404} status={404} />
-                <Route path='*' component={Page404} status={404} />
-              </Switch>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route exact path='/' render={props => <Landing {...props} />} />
+                  <Route
+                    path='/authenticate/:provider'
+                    render={props => <Auth {...props} />}
+                  />
+                  <Route path='/explore/:id' component={Landing} />
+                  <Route
+                    path='/welcome'
+                    render={props => <Landing {...props} newSignUp={true} />}
+                  />
+                  <Route
+                    path='/terms-and-conditions'
+                    exact
+                    component={TermsAndConditions}
+                  />
+                  <Route path='/privacy-policy' exact component={PrivacyPolicy} />
+                  <Route path='/about-us' exact component={AboutUs} />
+                  <Route path='/home' component={routeRedirectToProfile()} />
+                  <Route exact path='/password-reset' component={PasswordReset} />
+                  <Route
+                    path='/password_reset_confirm/:token'
+                    component={ConfirmPasswordReset}
+                  />
+                  <Route path='/mythangs' component={routeRequiresAuth(MyThangs)} />
+                  <Route
+                    path={['/search/:searchQuery', '/search']}
+                    component={SearchResults}
+                  />
+                  <Route path='/profile/:id' component={RedirectProfile} />
+                  <Route path='/u/:id' component={RedirectProfile} />
+                  <Route path='/model/:modelId' component={RedirectModel} />
+                  <Route path='/models/:modelId' component={RedirectModel} />
+                  <Route path='/m/:modelId' component={RedirectModel} />
+                  <Route
+                    path='/:userName/:modelString'
+                    render={props => <ModelDetail {...props} />}
+                  />
+                  <Route path='/:userName' component={Profile} />
+                  <Route path='/404' component={Page404} status={404} />
+                  <Route path='*' component={Page404} status={404} />
+                </Switch>
+              </Suspense>
             </OverlayProvider>
           </ActionMenuProvider>
         </ThemeProvider>
