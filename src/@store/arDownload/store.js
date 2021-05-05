9d57bb7e467897de4169bb66d2cfd201f248e763
Adam Gleichsner <agleichsner@physna.com>
2021-05-05 00:53:30 -0400

    HOTFIX - Fixing broken url for android intent, switching back to direct androidUrl field for now since signedUrl seems to break arcore

--- src/@store/arDownload/store.js
+++ src/@store/arDownload/store.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda'
 import axios from 'axios'
 
 import api from '@services/api'
@@ -106,28 +107,49 @@ export default store => {
         onFinish: async url => {
           // Native Android viewer requires the file to be loaded first, so this call forces us to generate the model if not already available
           if (format === 'android') {
-            let isGLBReady = false
-            try {
-              const { error } = await axios.get(url)
-              if (!error) {
-                isGLBReady = true
-              }
-            } catch (e) {
-              if (e.isAxiosError) {
-                isGLBReady = true
-              }
-            } finally {
-              if (isGLBReady) {
-                const link = document.createElement('a')
-                // One
-                link.href = `intent://arvr.google.com/scene-viewer/1.1?file=${encodeURIComponent(
-                  `${url}&cachebuster=${Date.now()}`
-                )}#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
+            const { parts } = model
+            let primaryPart
 
-                link.appendChild(document.createTextNode('Open intent'))
-                link.click()
+            if (parts) {
+              if (parts.length > 1) {
+                primaryPart = R.find(R.propEq('isPrimary', true))(parts) || parts[0]
               } else {
-                store.dispatch(types.FAILED_AR_DOWNLOAD, { mode: AR_MODE.VIEW, id })
+                primaryPart = parts[0]
+              }
+            }
+
+            if (!primaryPart || !primaryPart?.androidUrl) {
+              store.dispatch(types.FAILED_AR_DOWNLOAD, { mode: AR_MODE.VIEW, id })
+            } else {
+              let isGLBReady = false
+              try {
+                const { error } = await axios.get(url)
+                if (!error) {
+                  isGLBReady = true
+                }
+              } catch (e) {
+                if (e.isAxiosError) {
+                  isGLBReady = true
+                }
+              } finally {
+                if (isGLBReady) {
+                  // TODO - We ideally should be using the signed url from the endpoint but it seems to be breaking arcore
+                  const arCoreFriendlyUrl = `${
+                    primaryPart.androidUrl
+                  }?cachebuster=${Date.now()}`
+                    .replaceAll('?', encodeURIComponent('?'))
+                    .replaceAll('&', encodeURIComponent('&'))
+                    .replaceAll('#', encodeURIComponent('#'))
+
+                  const link = document.createElement('a')
+
+                  link.href = `intent://arvr.google.com/scene-viewer/1.1?file=${arCoreFriendlyUrl}#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
+
+                  link.appendChild(document.createTextNode('Open intent'))
+                  link.click()
+                } else {
+                  store.dispatch(types.FAILED_AR_DOWNLOAD, { mode: AR_MODE.VIEW, id })
+                }
               }
             }
           } else if (format === 'ios') {
